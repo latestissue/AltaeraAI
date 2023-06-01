307e7656a087d7e5fe71c6bc93a8cec654faf0a5
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-01 13:44:36 +0200

    Update altaera.sh

--- altaera.sh
+++ altaera.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd 'koboldcpp-altaera'
-python3 koboldcpp.py q5_1-RWKV-4-Raven-1B5-v12-Eng98%-Other2%-20230520-ctx4096.bin 1551
 termux-open-url http://localhost:1551
+python3 koboldcpp.py q5_1-RWKV-4-Raven-1B5-v12-Eng98%-Other2%-20230520-ctx4096.bin 1551
