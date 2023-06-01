34caf5801551e663db986bba7e5195585a239c30
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-01 12:10:55 +0200

    Update altaera script.sh

--- altaera script.sh
+++ altaera script.sh
@@ -1,11 +1,5 @@
 termux-change-repo
 termux-setup-storage
-pkg install git wget openssl-1.1 clang cmake python3 python-pip -y
-git clone https://github.com/latestissue/AltaeraAI
-cd 'AltaeraAI'
-wget https://huggingface.co/latestissue/rwkv-4-raven-ggml-quantized/resolve/main/q5_1-RWKV-4-Raven-1B5-v12-Eng98%25-Other2%25-20230520-ctx4096.bin
-make
-rm -rf '/data/data/com.termux/files/usr/etc/bash.bashrc'
-cp 'bash.bashrc' '/data/data/com.termux/files/usr/etc'
-cp 'altaera.sh' '$PREFIX'
-cd ..
+pkg install wget -y
+wget https://github.com/latestissue/altaera-packages.github.io/blob/main/altaera_script.sh
+bash 'altaera_script.sh'
