ccac154c5acb86aaa9d8cbea4f92396841aab87c
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-01 12:23:03 +0200

    Update altaera_script.sh

--- altaera_script.sh
+++ altaera_script.sh
@@ -3,9 +3,9 @@ git clone https://github.com/latestissue/AltaeraAI
 cd 'AltaeraAI'
 wget https://huggingface.co/latestissue/rwkv-4-raven-ggml-quantized/resolve/main/q5_1-RWKV-4-Raven-1B5-v12-Eng98%25-Other2%25-20230520-ctx4096.bin
 rm -rf 'klite.embd'
-wget https://github.com/latestissue/altaera-packages.github.io/blob/main/klite.embd
-wget https://github.com/latestissue/altaera-packages.github.io/blob/main/bash.bashrc
-wget https://github.com/latestissue/altaera-packages.github.io/blob/main/altaera.sh
+wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/klite.embd
+wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/bash.bashrc
+wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera.sh
 rm -rf '/data/data/com.termux/files/usr/etc/bash.rc'
 cp '~/AltaeraAI/bash.bashrc' '/data/data/com.termux/files/usr/etc/'
 cp '~/AltaeraAI/altaera.sh' '~/'
