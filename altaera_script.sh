39c946a60c8274b6fed09452529f3dcc10c15af6
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-01 12:30:23 +0200

    Update altaera_script.sh

--- altaera_script.sh
+++ altaera_script.sh
@@ -1,4 +1,6 @@
-pkg install git clang cmake python3 python-pip -y
+pkg install root-repo x11-repo git curl clang cmake python3 python-pip -y
+apt-get update
+apt-get upgrade -y
 git clone https://github.com/latestissue/AltaeraAI
 cd 'AltaeraAI'
 wget https://huggingface.co/latestissue/rwkv-4-raven-ggml-quantized/resolve/main/q5_1-RWKV-4-Raven-1B5-v12-Eng98%25-Other2%25-20230520-ctx4096.bin
