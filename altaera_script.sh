7ba3da64aa1623b1a658e78594d41faa319c87e4
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-01 13:55:06 +0200

    Update altaera_script.sh

--- altaera_script.sh
+++ altaera_script.sh
@@ -17,5 +17,6 @@ cd '/data/data/com.termux/files/home'
 rm -rf 'altaera_script.sh'
 chmod a+x 'altaera.sh'
 clear
-echo "Installation completed. You can now run AltaeraAI by writing 'altaera' - remember this command for future app launching.
-After running the command, you will be forwarded to your browser with the UI deployed at 'localhost:1551'."
+echo "Installation completed. Please restart Termux with the 'Exit' button from the notification bar.
+After that, you can run AltaeraAI by writing 'altaera' - remember this command for future app launching.
+After running the command, you will be forwarded to your browser with the UI deployed at 'localhost:1551'. If the website does not load, refresh the page."
