a744bfc046278237360f44071e11149fb87a977c
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-05 18:38:03 +0200

    Update altaera script.sh

--- altaera script.sh
+++ altaera script.sh
@@ -1,5 +1,5 @@
 termux-change-repo
 termux-setup-storage
-pkg install openssl-1.1 wget -y
+pkg -o DPkg::Options::="--force-confnew" -y install openssl-1.1 wget
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera_script.sh
 bash 'altaera_script.sh'
