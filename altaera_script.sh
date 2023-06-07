8559531be565725025b88aa9823d62f41ddc6861
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-07 15:55:01 +0200

    Update altaera_script.sh

--- altaera_script.sh
+++ altaera_script.sh
@@ -4,7 +4,7 @@ apt-get -o DPkg::Options::="--force-confnew" -y upgrade
 git clone https://github.com/LostRuins/koboldcpp
 mv 'koboldcpp' 'koboldcpp-altaera'
 cd 'koboldcpp-altaera'
-wget https://huggingface.co/latestissue/rwkv-4-raven-ggml-quantized/resolve/main/q8_0-RWKV-4-Raven-1B5-v12-Eng98%25-Other2%25-20230520-ctx4096.bin
+wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-1.5B-v1-20230607-ctx4096.bin
 make
 ##
 ##
