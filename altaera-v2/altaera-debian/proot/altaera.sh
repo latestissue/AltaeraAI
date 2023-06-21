fe6ca76685c167635978f47c993d449a45b152b0
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-21 19:43:14 +0200

    Update altaera.sh

--- altaera-v2/altaera-debian/proot/altaera.sh
+++ altaera-v2/altaera-debian/proot/altaera.sh
@@ -1,4 +1,4 @@
-DISTRO_NAME="AltaeraAI"
+DISTRO_NAME="Altaera"
 TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v1.10.1/debian-aarch64-pd-v1.10.1.tar.xz"
 TARBALL_SHA256['aarch64']="f34802fbb300b4d088a638c638683fd2bfc1c03f4b40fa4cb7d2113231401a21"
 
