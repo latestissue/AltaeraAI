ca51f131f38e3a1177349f776c575a417c47736d
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-21 19:28:41 +0200

    Update altaera_debian.sh

--- altaera-v2/altaera-debian/altaera_debian.sh
+++ altaera-v2/altaera-debian/altaera_debian.sh
@@ -3,6 +3,6 @@ TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download
 TARBALL_SHA256['aarch64']="f34802fbb300b4d088a638c638683fd2bfc1c03f4b40fa4cb7d2113231401a21"
 
 distro_setup() {
-	run_proot_cmd apt update
-	run_proot_cmd apt upgrade -yq
+	run_proot_cmd wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera_install_pt-2.sh
+ 	run_proot_cmd bash 'altaera_install_pt-2.sh'
 }
