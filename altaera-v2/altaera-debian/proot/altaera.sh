b0e9adea18f474c16239b3675c503282ccddf3dc
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-21 20:24:54 +0200

    Update altaera.sh

--- altaera-v2/altaera-debian/proot/altaera.sh
+++ altaera-v2/altaera-debian/proot/altaera.sh
@@ -3,7 +3,8 @@ TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download
 TARBALL_SHA256['aarch64']="f34802fbb300b4d088a638c638683fd2bfc1c03f4b40fa4cb7d2113231401a21"
 
 distro_setup() {
-	run_proot_cmd apt install ca-certificates
+	run_proot_cmd apt update && apt upgrade
+	run_proot_cmd apt install curl apt-transport-https ca-certificates ssl-cert
 	run_proot_cmd wget --no-check-certificate https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera_install_pt-2.sh
  	run_proot_cmd bash 'altaera_install_pt-2.sh'
 }
