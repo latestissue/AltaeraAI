db4e3b65e1874ea38b4d120bd689896b7ef71c94
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-21 19:30:41 +0200

    Update altaera_install.sh

--- altaera-v2/altaera_install.sh
+++ altaera-v2/altaera_install.sh
@@ -3,11 +3,7 @@ termux-setup-storage
 apt update && apt -o DPkg::Options::="--force-confnew" -y upgrade
 apt-get -o DPkg::Options::="--force-confnew" -y install openssl-1.1 wget
 pkg install wget curl proot-distro -y
-proot-distro install debian
-proot-distro rename debian altaera
-wget https://github.com/latestissue/altaera-packages.github.io/blob/main/altaera-v2/altaera-debian/initial/bash.bashrc
-wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/initial/initial.sh
-rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/etc/bash.bashrc'
-cp 'bash.bashrc' '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/etc'
-cp 'initial.sh' '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
-proot-distro login altaera
+cd $PREFIX/etc/proot-distro
+wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/altaera_debian.sh
+proot-distro install altaera_debian.sh
+proot-distro login altaera_debian
