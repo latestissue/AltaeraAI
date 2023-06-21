43c458efb99faa996ae8bbd6b31380858a3c28c5
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-21 19:39:01 +0200

    Update altaera_install.sh

--- altaera-v2/altaera_install.sh
+++ altaera-v2/altaera_install.sh
@@ -4,6 +4,6 @@ apt update && apt -o DPkg::Options::="--force-confnew" -y upgrade
 apt-get -o DPkg::Options::="--force-confnew" -y install openssl-1.1 wget
 pkg install wget curl proot-distro -y
 cd $PREFIX/etc/proot-distro
-wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/altaera_debian.sh
-proot-distro install altaera_debian.sh
-proot-distro login altaera_debian
+wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/proot/altaera.sh
+proot-distro install altaera
+proot-distro login altaera
