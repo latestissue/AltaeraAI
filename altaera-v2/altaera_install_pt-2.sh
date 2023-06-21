2ff1d6e3f77458fec93dc70ca9bea8475e999680
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-21 15:31:13 +0200

    Update altaera_install_pt-2.sh

--- altaera-v2/altaera_install_pt-2.sh
+++ altaera-v2/altaera_install_pt-2.sh
@@ -10,8 +10,8 @@ cp 'bash.bashrc' '/data/data/com.termux/files/usr/etc/'
 ln -rs $PREFIX/var/lib/proot-distro/installed-rootfs
 cd $PREFIX/var/lib/proot-distro/installed-rootfs
 wget https://download1523.mediafire.com/m2jz6ebi90zgOTl3fBOMH3cGmUh4nMyLQu8Rkrs4XayH9YbkImazqQGOrcu9fCPPmFJ1zjT4at7Uuze5WF5ss7fujhPRmc9MxGLvgOrPqHd5lrUN7-83QLRsDpVEtnQtbwLWXe3RcPxt1wrJU5mOSeH-Ox_blb6qxU73AAgIAE-GREQ/zfm44ffur7262iv/altaera-v2.tar
-tar xvf 'altaera-v2'
-rm -rf 'altaera-v2'
+tar xvf 'altaera-v2.tar'
+rm -rf 'altaera-v2.tar'
 cd /data/data/com.termux/files/home
 rm -rf 'altaera_install.sh'
 rm -rf 'altaera_install_pt-2.sh
