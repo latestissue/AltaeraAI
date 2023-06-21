af70da73077c2c7c2a646ff12f96f26673ed1da9
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-21 17:10:19 +0200

    Update altaera_install_pt-2.sh

--- altaera-v2/altaera_install_pt-2.sh
+++ altaera-v2/altaera_install_pt-2.sh
@@ -1,7 +1,12 @@
+wget https://github.com/latestissue/altaera-packages.github.io/raw/main/altaera-v2/altaera-v2.tar.gz
+tar -xf 'altaera-v2.tar.gz'
+rm -rf 'altaera-v2.tar.gz'
+cd koboldcpp-altaera
+wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin
+logout
 cd /data/data/com.termux/files/home
 rm -rf 'altaera_install.sh'
 rm -rf 'altaera_install_pt-2.sh
-logout
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-update.sh
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera.sh
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/bash.bashrc
@@ -9,6 +14,7 @@ chmod a+x 'altaera-update.sh'
 chmod a+x 'altaera.sh'
 rm -rf '/data/data/com.termux/files/usr/etc/bash.rc'
 cp 'bash.bashrc' '/data/data/com.termux/files/usr/etc/'
+rm -rf 'bash.bashrc'
 clear
 echo "Installation completed. Please restart Termux with the 'Exit' button from the notification bar.
 After that, you can run AltaeraAI by writing 'altaera' - remember this command for future app launching.
