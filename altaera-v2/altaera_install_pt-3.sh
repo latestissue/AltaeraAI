e1070d804c40e0103b5fd54fe29505956e2261d4
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-22 13:41:10 +0200

    Update altaera_install_pt-3.sh

--- altaera-v2/altaera_install_pt-3.sh
+++ altaera-v2/altaera_install_pt-3.sh
@@ -13,13 +13,19 @@ cd prompts
 rm -rf 'chat-with-bob.txt'
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/prompts/chat-with-bob.txt
 cd /root
-apt install clang python3 python3-pip libclblast-dev libopenblas-dev -y
+apt install clang python3 libclblast-dev libopenblas-dev -y
 clear
 echo "
 
               **********INSTALLATION COMPLETED**********
 
 _______________________________________________________________________
+
+      !!!  Please restart Termux now, by pulling down the notification bar, then expanding 'Termux' and pressing 'Exit'.  !!!
+      !!!                  (You can also remove Termux from the 'apps switch' list, just to make sure)                    !!!
+
+      
+      --- Then, open Termux and:
 
 
 - To start, please type in 'altaera'.
