7e0a4ca5f648f9fd79c09346503f8025e79817e7
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-22 12:13:08 +0200

    Update altaera_install_pt-3.sh

--- altaera-v2/altaera_install_pt-3.sh
+++ altaera-v2/altaera_install_pt-3.sh
@@ -17,14 +17,14 @@ apt install clang python3 python3-pip libclblast-dev libopenblas-dev -y
 clear
 echo "
 
-              **********Installation completed**********
+              **********INSTALLATION COMPLETED**********
 
 _______________________________________________________________________
-Please restart Termux with the 'Exit' button from the notification bar.
 
-After that, you can run AltaeraAI by writing 'altaera' - remember this command for future app launching.
 
-After running the command, GO BACK to the AltaeraAI apk and tap on the 'HTTPS://ALTAERA.AI/LOCALHOST/' button, you will be welcomed with front-end UI. If the website does not load, please refresh the page.
+GO BACK to the AltaeraAI apk and tap on the 'HTTPS://ALTAERA.AI/LOCALHOST/' button, you will be welcomed with front-end UI. If the website does not load, please refresh the page.
+
+
 _______________________________________________________________________
 
 (IGNORE the text on the bottom)
