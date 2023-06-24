00bdf952ac93e74a0f0dc48c15bc7894c7b73916
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-24 15:32:54 +0200

    Update altaera_install_pt-4.sh

--- altaera-v2/altaera_install_pt-4.sh
+++ altaera-v2/altaera_install_pt-4.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-cd prompts
-rm -rf 'chat-with-bob.txt'
-
 HEIGHT=15
 WIDTH=40
 CHOICE_HEIGHT=4
@@ -25,38 +22,19 @@ clear
 case $CHOICE in
         1)
             echo "You chose English"
-            wget wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/prompts/chat-with-bob.txt
+            cd prompts
+            rm -rf 'chat-with-bob.txt'
+            wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/prompts/chat-with-bob.txt
             ;;
         2)
-            echo "You chose Polish"
-            wget https://huggingface.co/latestissue/rwkv-4-world-ggml/resolve/main/f16-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin
+            echo "You chose English"
+            cd prompts
+            rm -rf 'chat-with-bob.txt'
+            wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/prompts/Polish/chat-with-bob.txt
             ;;
 esac
-
-cd /root
-clear
-echo "
-
-              **********INSTALLATION COMPLETED**********
-_______________________________________________________________________
-
-
-      !!!  Please restart Termux now, by pulling down the notification bar, then expanding 'Termux' and pressing 'Exit'.  !!!
-      !!!                  (You can also remove Termux from the 'apps switch' list, just to make sure)                    !!!
-
-      
-      --- Then, open Termux and:
-
-
-- To start, please type in 'altaera'.
-
-- Then, GO BACK to the AltaeraAI apk and tap on the 'HTTPS://ALTAERA.AI/LOCALHOST/' button, you will be welcomed with front-end UI. If the website does not load, please refresh the page.
-
-
-_______________________________________________________________________
-
-(IGNORE the text on the bottom)
-              |
-              v
-              "
               
+cd /root
+wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera_install_pt-5.sh
+chmod a+x 'altaera_install_pt-5.sh'
+bash 'altaera_install_pt-5.sh'
