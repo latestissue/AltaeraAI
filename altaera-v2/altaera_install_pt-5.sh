8ec50fd80edbff871a8f1852c81bbcf1cdbad9b2
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-24 15:30:32 +0200

    Create altaera_install_pt-5.sh

---
+++ altaera-v2/altaera_install_pt-5.sh
@@ -0,0 +1,26 @@
+clear
+echo "
+
+              **********INSTALLATION COMPLETED**********
+_______________________________________________________________________
+
+
+      !!!  Please restart Termux now, by pulling down the notification bar, then expanding 'Termux' and pressing 'Exit'.  !!!
+      !!!                  (You can also remove Termux from the 'apps switch' list, just to make sure)                    !!!
+
+      
+      --- Then, open Termux and:
+
+
+- To start, please type in 'altaera'.
+
+- Then, GO BACK to the AltaeraAI apk and tap on the 'HTTPS://ALTAERA.AI/LOCALHOST/' button, you will be welcomed with front-end UI. If the website does not load, please refresh the page.
+
+
+_______________________________________________________________________
+
+(IGNORE the text on the bottom)
+              |
+              v
+              "
+              
