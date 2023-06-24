1fb079b5c225de8c625c17e7da015955de6a023e
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-24 19:06:06 +0200

    Update altaera_install_pt-3.sh

--- altaera-v2/altaera_install_pt-3.sh
+++ altaera-v2/altaera_install_pt-3.sh
@@ -4,6 +4,8 @@ cp 'bash.bashrc' "/etc"
 rm -rf 'bash.bashrc'
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/altaera.sh
 chmod a+x 'altaera.sh'
+wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/lang.sh
+chmod a+x 'lang.sh'
 wget https://github.com/latestissue/altaera-packages.github.io/raw/main/altaera-v2/altaera-v2.tar.gz
 tar -xf 'altaera-v2.tar.gz'
 rm -rf 'altaera-v2.tar.gz'
