29fa28fc4684b842c064733697b3a1a89419834e
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-24 19:09:15 +0200

    Update altaera_install_pt-2.sh

--- altaera-v2/altaera_install_pt-2.sh
+++ altaera-v2/altaera_install_pt-2.sh
@@ -2,9 +2,11 @@ cd /data/data/com.termux/files/home
 rm -rf 'altaera_install.sh'
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-update.sh
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera.sh
+wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-lang.sh
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/bash.bashrc
 chmod a+x 'altaera-update.sh'
 chmod a+x 'altaera.sh'
+chmod a+x 'altaera-lang.sh'
 rm -rf '/data/data/com.termux/files/usr/etc/bash.rc'
 cp 'bash.bashrc' '/data/data/com.termux/files/usr/etc/'
 rm -rf 'bash.bashrc'
