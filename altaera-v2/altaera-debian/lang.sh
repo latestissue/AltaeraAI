b9af8b3c7a02c052a9bc42cfbb1e2fd1067fc6df
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-26 12:44:03 +0200

    Update lang.sh

--- altaera-v2/altaera-debian/lang.sh
+++ altaera-v2/altaera-debian/lang.sh
@@ -8,7 +8,8 @@ TITLE="Please choose which in language you would like to talk to with the AI"
 MENU="Choose one of the following options:"
 
 OPTIONS=(1 "English [Default]"
-         2 "Polish")
+         2 "Chinese"
+         3 "Polish")
 
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
@@ -28,7 +29,14 @@ case $CHOICE in
             cd /root
             ;;
         2)
-            echo "You chose English"
+            echo "你选择了中文"
+            cd '/root/koboldcpp-altaera/prompts'
+            rm -rf 'chat-with-bob.txt'
+            wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/prompts/Chinese/chat-with-bob.txt
+            cd /root
+            ;;
+        3)
+            echo "Wybrałeś język polski"
             cd '/root/koboldcpp-altaera/prompts'
             rm -rf 'chat-with-bob.txt'
             wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/prompts/Polish/chat-with-bob.txt
