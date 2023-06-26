d4dab82e401b953a9b092d328eca6e874b44a530
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-26 12:34:47 +0200

    Update altaera-update_content.sh

--- altaera-v2/altaera-update_content.sh
+++ altaera-v2/altaera-update_content.sh
@@ -1,1 +1,7 @@
-###
+rm -rf 'lang.sh'
+wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/lang.sh
+chmod a+x 'lang.sh'
+rm -rf 'bash.bashrc'
+rm -rf "/etc/bash.bashrc"
+wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/bash.bashrc
+cp 'bash.bashrc' "/etc"
