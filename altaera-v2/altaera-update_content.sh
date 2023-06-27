e44142ab071f01f51e9587668adf1e8ca4968250
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-27 16:50:16 +0200

    Update altaera-update_content.sh

--- altaera-v2/altaera-update_content.sh
+++ altaera-v2/altaera-update_content.sh
@@ -10,3 +10,14 @@ cd /etc
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/bash.bashrc
 cd '/data/data/com.termux/files/home'
 rm -rf 'bash.bashrc'
+cd '$PREFIX/var/lib/proot-distro/installed-rootfs/altaera/root/koboldcpp-altaera'
+rm -rf 'klite.embd'
+wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/klite/klite.embd
+cd '/data/data/com.termux/files/home'
+clear
+echo "
+
+
+      Update completed!
+      "
+
