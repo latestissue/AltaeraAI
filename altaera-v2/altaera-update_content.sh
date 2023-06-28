453fd32771817ad39aa6964a9294512a6867d2fd
latestissue <121747280+latestissue@users.noreply.github.com>
2023-06-28 14:44:42 +0200

    Update altaera-update_content.sh

--- altaera-v2/altaera-update_content.sh
+++ altaera-v2/altaera-update_content.sh
@@ -14,6 +14,10 @@ cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaer
 rm -rf 'klite.embd'
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/klite/klite.embd
 mv 'q8_0-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin' 'RWKV-model.bin'
+cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
+rm -rf 'altaera.sh'
+wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/altaera.sh
+chmod a+x 'altaera.sh'
 cd '/data/data/com.termux/files/home'
 clear
 echo "
