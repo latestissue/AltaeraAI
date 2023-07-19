c5f0aa9e6f185fef4d0922baaaa4554b95a964e3
latestissue <121747280+latestissue@users.noreply.github.com>
2023-07-19 11:26:45 +0200

    Update altaera_install_pt-3.sh

--- altaera-v2/altaera_install_pt-3.sh
+++ altaera-v2/altaera_install_pt-3.sh
@@ -4,9 +4,9 @@ cp 'bash.bashrc' "/etc"
 rm -rf 'bash.bashrc'
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/altaera.sh
 chmod a+x 'altaera.sh'
-wget https://github.com/latestissue/altaera-packages.github.io/releases/download/v2.1/altaera-v2.1.tar.gz
-tar -xf 'altaera-v2.1.tar.gz'
-rm -rf 'altaera-v2.1.tar.gz'
+wget https://github.com/latestissue/altaera-packages.github.io/releases/download/v2.2/altaera-v2.2.tar.gz
+tar -xf 'altaera-v2.2.tar.gz'
+rm -rf 'altaera-v2.2.tar.gz'
 cd koboldcpp-altaera
 wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin
 mv 'q8_0-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin' 'model.bin'
