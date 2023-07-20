eb26bde85c54b38368b57b9453a31f637394b22d
latestissue <121747280+latestissue@users.noreply.github.com>
2023-07-20 13:02:27 +0200

    Update test.sh

--- altaera-v2/test.sh
+++ altaera-v2/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $(vmstat -s | grep -i 'total memory' | sed 's/ *//'
-) = "11712136 K total memory" ]; then
+if [ $(cat /proc/meminfo | head -n 3
+) = "MemTotal:        11712136 kB" ]; then
 	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-3B-v1-20230619-ctx4096.bin
   mv 'q8_0-RWKV-4-World-3B-v1-20230619-ctx4096.bin' 'model.bin'
 else
