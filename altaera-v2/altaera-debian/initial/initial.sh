84bce1aa4e3e36a19b80d8f0f725c240db878a22
latestissue <121747280+latestissue@users.noreply.github.com>
2023-07-20 14:45:43 +0200

    Update initial.sh

--- altaera-v2/altaera-debian/initial/initial.sh
+++ altaera-v2/altaera-debian/initial/initial.sh
@@ -1,3 +1,3 @@
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera_install_pt-3.sh
 chmod a+x 'altaera_install_pt-3.sh'
-bash altaera_install_pt-3.sh
+./altaera_install_pt-3.sh
