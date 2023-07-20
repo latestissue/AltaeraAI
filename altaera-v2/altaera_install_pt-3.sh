5af492ab30ead8d7165316c7237ac290835a2415
latestissue <121747280+latestissue@users.noreply.github.com>
2023-07-20 13:37:43 +0200

    Update altaera_install_pt-3.sh

--- altaera-v2/altaera_install_pt-3.sh
+++ altaera-v2/altaera_install_pt-3.sh
@@ -8,10 +8,34 @@ wget https://github.com/latestissue/altaera-packages.github.io/releases/download
 tar -xf 'altaera-v2.2.tar.gz'
 rm -rf 'altaera-v2.2.tar.gz'
 cd koboldcpp-altaera
-if [ $(vmstat -s | grep -i 'total memory' | sed 's/ *//'
-) = '11712136 K total memory' ]; then
+if [ $(free -h | awk '/Mem\:/ { print $2 }'
+) = "15Gi" ]; then
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-7B-v1-20230626-ctx4096.bin
+  	mv 'q8_0-RWKV-4-World-7B-v1-20230626-ctx4096.bin' 'model.bin'
+elif [ $(free -h | awk '/Mem\:/ { print $2 }'
+) = "11Gi" ]; then
 	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-3B-v1-20230619-ctx4096.bin
   mv 'q8_0-RWKV-4-World-3B-v1-20230619-ctx4096.bin' 'model.bin'
+elif [ $(free -h | awk '/Mem\:/ { print $2 }'
+) = "7Gi" ]; then
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q5_1-RWKV-4-World-3B-v1-20230619-ctx4096.bin
+  mv 'q5_1-RWKV-4-World-3B-v1-20230619-ctx4096.bin' 'model.bin'
+elif [ $(free -h | awk '/Mem\:/ { print $2 }'
+) = "5Gi" ]; then
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q5_1-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin
+  mv 'q5_1-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin' 'model.bin'
+elif [ $(free -h | awk '/Mem\:/ { print $2 }'
+) = "3Gi" ]; then
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q4_1-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin
+  mv 'q4_1-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin' 'model.bin'
+  elif [ $(free -h | awk '/Mem\:/ { print $2 }'
+) = "2Gi" ]; then
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml/resolve/main/f32-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin
+  mv 'f32-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin' 'model.bin'
+    elif [ $(free -h | awk '/Mem\:/ { print $2 }'
+) = "1Gi" ]; then
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml/resolve/main/f16-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin
+  mv 'f16-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin' 'model.bin'
 else
   wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin
   mv 'q8_0-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin' 'model.bin'
