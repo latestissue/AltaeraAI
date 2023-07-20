d282b17198fd123a732c7c9bc6fa7a435a30304b
latestissue <121747280+latestissue@users.noreply.github.com>
2023-07-20 13:57:08 +0200

    Update altaera_install_pt-3.sh

--- altaera-v2/altaera_install_pt-3.sh
+++ altaera-v2/altaera_install_pt-3.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 rm -rf "/etc/bash.bashrc"
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/bash.bashrc
 cp 'bash.bashrc' "/etc"
@@ -36,6 +38,9 @@ elif [ $(free -h | awk '/Mem\:/ { print $2 }'
 ) = "1Gi" ]; then
 	wget https://huggingface.co/latestissue/rwkv-4-world-ggml/resolve/main/f16-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin
   mv 'f16-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin' 'model.bin'
+  else
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin
+  	mv 'q8_0-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin' 'model.bin'
 fi
 cd prompts
 rm -rf 'chat-with-bob.txt'
