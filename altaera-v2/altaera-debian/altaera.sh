9bef88a87beae04032715c0eb7b37ba20ffe7e5d
latestissue <121747280+latestissue@users.noreply.github.com>
2023-08-09 10:26:18 +0200

    Update altaera.sh

--- altaera-v2/altaera-debian/altaera.sh
+++ altaera-v2/altaera-debian/altaera.sh
@@ -2,4 +2,4 @@
 
 cd "koboldcpp-altaera"
 termux-open-url 'http://localhost:1551'
-python3 koboldcpp.py model.bin 1551 --smartcontext --blasbatchsize 2048 --contextsize 512 --threads 8 --highpriority
+python3 koboldcpp.py model.bin 1551 --smartcontext --blasbatchsize 2048 --contextsize 512 
