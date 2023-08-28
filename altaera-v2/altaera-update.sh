20340eb4da5ef5431d5bc14a939affdb507adc27
latestissue <121747280+latestissue@users.noreply.github.com>
2023-08-28 12:53:12 +0200

    Update altaera-update.sh

--- altaera-v2/altaera-update.sh
+++ altaera-v2/altaera-update.sh
@@ -1,6 +1,9 @@
+echo "...";
+
 {
 rm -rf 'altaera-update_content.sh'
 wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-update_content.sh
 chmod a+x 'altaera-update_content.sh'
 bash 'altaera-update_content.sh'
 } &> /dev/null 2>&1;
+clear
