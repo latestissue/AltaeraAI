726edb2ac9f7bc7645e491f78b408f4431370732
latestissue <121747280+latestissue@users.noreply.github.com>
2023-08-30 11:25:15 +0200

    Create altaera_spa.sh

---
+++ altaera-v2/altaera_spa.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+HEIGHT=30
+WIDTH=60
+CHOICE_HEIGHT=7
+BACKTITLE="AltaeraAI"
+TITLE="Bienvenido/a"
+MENU="¿Qué te gustaría hacer?"
+
+OPTIONS=(1 "Iniciar AltaeraAI"
+         2 "Actualizar AltaeraAI"
+         3 "Cambiar el modelo de AI 🤖"
+         4 "Cambiar el idioma 🌐"
+         5 "Salir de")
+         
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            clear
+            proot-distro login altaera -- ./altaera.sh &
+            ;;
+        2)
+            clear
+            ./altaera-update.sh
+            ./altaera.sh
+            ;;
+        3)
+            clear
+            ./altaera-model.sh
+            ./altaera.sh
+            ;;
+        4)
+            clear
+            ./altaera-lang.sh
+            ./altaera.sh
+            ;;
+         5)
+            clear
+            ;;
+esac
