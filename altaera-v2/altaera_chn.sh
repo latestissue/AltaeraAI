6b0920ab74e1a0c7cc0ab3364f8bd72434878011
latestissue <121747280+latestissue@users.noreply.github.com>
2023-08-30 11:06:56 +0200

    Create altaera_chn.sh

---
+++ altaera-v2/altaera_chn.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+HEIGHT=30
+WIDTH=60
+CHOICE_HEIGHT=7
+BACKTITLE="AltaeraAI"
+TITLE="欢迎光临!"
+MENU="你想做什么？"
+
+OPTIONS=(1 "启动 AltaeraAI"
+         2 "更新 AltaeraAI"
+         3 "改变人工智能模式 🤖"
+         4 "更改语言 🌐"
+         5 "退出")
+         
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            clear
+            proot-distro login altaera -- ./altaera.sh &
+            ;;
+        2)
+            clear
+            ./altaera-update.sh
+            ./altaera.sh
+            ;;
+        3)
+            clear
+            ./altaera-model.sh
+            ./altaera.sh
+            ;;
+        4)
+            clear
+            ./altaera-lang.sh
+            ./altaera.sh
+            ;;
+         5)
+            clear
+            ;;
+esac
