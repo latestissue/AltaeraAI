9f8eb72ddd532f40777e4b05a7df53d8f49196a6
latestissue <121747280+latestissue@users.noreply.github.com>
2023-09-05 12:29:59 +0200

    Update altaera_install_pt-3.sh

--- altaera-v2/altaera_install_pt-3.sh
+++ altaera-v2/altaera_install_pt-3.sh
@@ -4,18 +4,18 @@ echo "Installing core files...";
 
 {
 rm -rf "/etc/bash.bashrc"
-wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/bash.bashrc
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera-debian/bash.bashrc
 cp 'bash.bashrc' "/etc"
 rm -rf 'bash.bashrc'
-wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/altaera.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera-debian/altaera.sh
 chmod a+x 'altaera.sh'
 wget https://github.com/latestissue/altaera-packages.github.io/releases/download/v2.5/altaera-v2.5.tar.gz
 tar -xf 'altaera-v2.5.tar.gz'
 rm -rf 'altaera-v2.5.tar.gz'
 cd 'koboldcpp-altaera'
 rm -rf 'klite.embd'
-wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/klite/klite.embd
-wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera_install_pt-4.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera-debian/klite/klite.embd
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera_install_pt-4.sh
 chmod a+x 'altaera_install_pt-4.sh'
 } &> /dev/null 2>&1;
 ./altaera_install_pt-4.sh
