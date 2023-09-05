459fda87dc5685b46efb516cddc7696746259520
latestissue <121747280+latestissue@users.noreply.github.com>
2023-09-05 12:33:18 +0200

    Update altaera_install_pt-3.sh

--- altaera-v2/altaera_install_pt-3.sh
+++ altaera-v2/altaera_install_pt-3.sh
@@ -9,7 +9,7 @@ cp 'bash.bashrc' "/etc"
 rm -rf 'bash.bashrc'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera-debian/altaera.sh
 chmod a+x 'altaera.sh'
-wget https://github.com/latestissue/altaera-packages.github.io/releases/download/v2.5/altaera-v2.5.tar.gz
+wget https://github.com/latestissue/AltaeraAI/releases/download/v2.5/altaera-v2.5.tar.gz
 tar -xf 'altaera-v2.5.tar.gz'
 rm -rf 'altaera-v2.5.tar.gz'
 cd 'koboldcpp-altaera'
