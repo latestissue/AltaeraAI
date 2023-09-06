2354cbe82821527a7daa2c622e00693630748da4
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-06 12:03:54 +0200

    Create altaera-uninstall.sh

---
+++ altaera-v2/altaera-uninstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+dialog --title "Uninstall Altaera AI" \
+--backtitle "Linux Shell Script Tutorial Example" \
+--yesno "Are you sure you want to uninstall AltaeraAI?" 7 60
+
+response=$?
+case $response in
+   0)   proot-distro remove altaera
+        cd /data/data/com.termux/files/home
+        rm -rf 'bash.bashrc'
+        rm -rf 'altaera-lang.sh'
+        rm -rf 'altaera-update.sh'
+        rm -rf 'altaera_install_pt-2.sh'
+        rm -rf 'altaera-model.sh
+        rm -rf 'altaera.sh'
+        rm -rf 'install'
+        clear
+        echo "AltaeraAI has been succesfully uninstalled";;
+   1) ./altaera.sh;;
+   255) ./altaera.sh;;
+esac
