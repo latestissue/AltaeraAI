2938d3f62a5f618239b07b644c36573145c1bd6d
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-07 17:09:39 +0200

    Update altaera_install_pt-1-auto.sh

--- altaera-v2/altaera_install_pt-1-auto.sh
+++ altaera-v2/altaera_install_pt-1-auto.sh
@@ -2,6 +2,8 @@
 
 clear
 
+cd 'AltaeraAI-temp'
+
 echo "Downloading the AI model..."
 
    if [ $(free -h | awk '/Mem\:/ { print $2 }'
@@ -36,5 +38,7 @@ echo "Downloading the AI model..."
 	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin -q --show-progress
   mv 'q8_0-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin' 'model.bin'
 fi
+
+cd ..
 
 clear
