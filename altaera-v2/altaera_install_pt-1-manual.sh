129fa6fc56bbf9c16130d8483bf8155a3f27b913
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-07 16:52:16 +0200

    Update altaera_install_pt-1-manual.sh

--- altaera-v2/altaera_install_pt-1-manual.sh
+++ altaera-v2/altaera_install_pt-1-manual.sh
@@ -630,6 +630,4 @@ fi
             ;;
 esac
 
-wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera_install_pt-2.sh
-chmod a+x 'altaera_install_pt-2.sh'
-./altaera_install_pt-2.sh
+clear
