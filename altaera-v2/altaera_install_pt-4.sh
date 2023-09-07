5d3d160bd3e004ab323287762ecf881d4e26969c
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-07 14:57:21 +0200

    Update altaera_install_pt-4.sh

--- altaera-v2/altaera_install_pt-4.sh
+++ altaera-v2/altaera_install_pt-4.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-clear
-echo "Installing dependencies...";
-
-{
-cd 'prompts'
-rm -rf 'chat-with-bob.txt'
-wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera-debian/prompts/chat-with-bob.txt
-cd /root
-apt install clang python3 libclblast-dev libopenblas-dev -y
-} &> /dev/null 2>&1;
 
 HEIGHT=30
 WIDTH=60
