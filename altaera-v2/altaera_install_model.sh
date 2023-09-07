026d222f98b3ec4e754e4c3f095245a012a92404
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-07 15:39:29 +0200

    Update altaera_install_model.sh

--- altaera-v2/altaera_install_model.sh
+++ altaera-v2/altaera_install_model.sh
@@ -23,12 +23,12 @@ clear
 case $CHOICE in
         1)
             wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera_install_pt-1-auto.sh -q --show-progress
-            chmod a+x 'altaera_install_pt-6-auto.sh'
-            ./altaera_install_pt-6-auto.sh
+            chmod a+x 'altaera_install_pt-1-auto.sh'
+            ./altaera_install_pt-1-auto.sh
             ;;
         2)
             wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera_install_pt-1-manual.sh
-            chmod a+x 'altaera_install_pt-6-manual.sh'
-            ./altaera_install_pt-6-manual.sh
+            chmod a+x 'altaera_install_pt-1-manual.sh'
+            ./altaera_install_pt-1-manual.sh
             ;;
 esac
