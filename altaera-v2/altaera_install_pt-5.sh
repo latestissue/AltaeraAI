0f989efe3ee8e5102dfffee79acc9ecac54fb04c
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-07 14:58:36 +0200

    Update altaera_install_pt-5.sh

--- altaera-v2/altaera_install_pt-5.sh
+++ altaera-v2/altaera_install_pt-5.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
-
-HEIGHT=30
-WIDTH=60
-CHOICE_HEIGHT=7
-BACKTITLE="AltaeraAI"
-TITLE="Installing AltaeraAI - AI model"
-MENU="Choose your desired method"
-
-OPTIONS=(1 "AI model auto-detection [default]"
-         7 "AI model manual selection")
-         
-
-CHOICE=$(dialog --clear \
-                --backtitle "$BACKTITLE" \
-                --title "$TITLE" \
-                --menu "$MENU" \
-                $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                "${OPTIONS[@]}" \
-                2>&1 >/dev/tty)
-
 clear
-case $CHOICE in
-        1)
-            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera_install_pt-6-auto.sh -q --show-progress
-            chmod a+x 'altaera_install_pt-6-auto.sh'
-            ./altaera_install_pt-6-auto.sh
-            ;;
-        2)
-            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera_install_pt-6-manual.sh
-            chmod a+x 'altaera_install_pt-6-manual.sh'
-            ./altaera_install_pt-6-manual.sh
-            ;;
-esac
+echo "Installing dependencies...";
+
+{
+cd 'prompts'
+rm -rf 'chat-with-bob.txt'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera-debian/prompts/chat-with-bob.txt
+cd /root
+apt install clang python3 libclblast-dev libopenblas-dev -y
+} &> /dev/null 2>&1;
+
+cd /root
+clear
+echo "
+
+              **********INSTALLATION COMPLETED**********
+_______________________________________________________________________
+
+
+      !!!  Please pull down the notification bar, expand 'Termux' and press 'Exit'  !!!
+
+      
+      --- Then, open Termux and:
+
+
+- To start, please type in 'ae'.
+
+
+_______________________________________________________________________
+
+(IGNORE the text on the bottom)
+              |
+              v
+              "
