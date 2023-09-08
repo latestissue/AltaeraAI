fcef6afa5fb08ccadef0c21a86393e4f1d0425aa
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-08 12:53:39 +0200

    Update altaera_install_pt-1-manual.sh

--- altaera-v2/altaera_install_pt-1-manual.sh
+++ altaera-v2/altaera_install_pt-1-manual.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+clear
+
 HEIGHT=120
 WIDTH=160
 CHOICE_HEIGHT=24
@@ -7,41 +9,42 @@ BACKTITLE="Installing AltaeraAI - AI model"
 TITLE="AI Model Selection"
 MENU="Choose your desired model:"
 
-OPTIONS=(1 "F16-RWKV-4-World-0.1B-Untuned       0.39GB / min. 1.5GB RAM"
-         2 "F32-RWKV-4-World-0.1B-Untuned       0.78GB / min. 3GB RAM"
-         3 "F16-RWKV-4-World-0.4B-Untuned       0.92GB / min. 4GB RAM"
-         4 "F32-RWKV-4-World-0.4B-Untuned       1.85GB / min. 6GB RAM"
-         5 "Q4_0-RWKV-4-World-1.5B-Untuned      1.19GB / min. 4GB RAM"
-         6 "Q4_0-RWKV-4-World-1.5B-CHNtuned     1.69GB / min. 6GB RAM"
-         7 "Q4_1-RWKV-4-World-1.5B-Untuned      1.26GB / min. 4GB RAM"
-         8 "Q4_1-RWKV-4-World-1.5B-CHNtuned     1.76GB / min. 6GB RAM"
-         9 "Q5_0-RWKV-4-World-1.5B-Untuned      1.34GB / min. 4/6GB RAM"
-         10 "Q5_0-RWKV-4-World-1.5B-CHNtuned     1.84GB / min. 6GB RAM"
-         11 "Q5_1-RWKV-4-World-1.5B-Untuned      1.42GB / min. 6GB RAM"
-         12 "Q5_1-RWKV-4-World-1.5B-CHNtuned     1.92GB / min. 6GB RAM"
-         13 "Q8_0-RWKV-4-World-1.5B-Untuned     1.8GB / min. 6GB RAM"
-         14 "Q8_0-RWKV-4-World-1.5B-CHNtuned    2.3GB / min. 8GB RAM"
-         15 "Q4_0-RWKV-4-World-3B-Untuned       2.06GB / min. 8GB RAM"
-         16 "Q4_0-RWKV-4-World-3B-CHNtuned      2.06GB / min. 8GB RAM"
-         17 "Q4_1-RWKV-4-World-3B-Untuned       2.22GB / min. 8GB RAM"
-         18 "Q4_1-RWKV-4-World-3B-CHNtuned      2.22GB / min. 8GB RAM"
-         19 "Q5_0-RWKV-4-World-3B-Untuned       2.37GB / min. 8GB RAM"
-         20 "Q5_0-RWKV-4-World-3B-CHNtuned      2.37GB / min. 8GB RAM"
-         21 "Q5_1-RWKV-4-World-3B-Untuned       2.53GB / min. 8/12GB RAM"
-         22 "Q5_1-RWKV-4-World-3B-CHNtuned      2.53GB / min. 8/12GB RAM"
-         23 "Q8_0-RWKV-4-World-3B-Untuned       3.33GB / min. 12GB RAM"
-         24 "Q8_0-RWKV-4-World-3B-CHNtuned      3.33GB / min. 12GB RAM"
-         25 "Q4_0-RWKV-4-World-7B-Untuned       4.66GB / min. 12GB RAM"
-         26 "Q4_1-RWKV-4-World-7B-Untuned       5.07GB / min. 12GB RAM"
-         27 "Q5_0-RWKV-4-World-7B-Untuned       5.47GB / min. 12GB RAM"
-         28 "Q5_1-RWKV-4-World-7B-Untuned       5.88GB / min. 12/16GB RAM"
-         29 "Q8_0-RWKV-4-World-7B-Untuned       7.91GB / min. 16GB RAM"
-         30 "Q4_0-open-llama-3b                1.93 GB / min. ???"
-         31 "Q4_1-open-llama-3b                2.14 GB / min. ???"
-         32 "Q5_0-open-llama-3b                2.36 GB / min. ???"
-         33 "Q5_1-open-llama-3b                2.57 GB / min. ???"
-         34 "Q8_0-open-llama-3b                3.64 GB / min. ???"
-         35 "F16-open-llama-3b                  6.85 GB / min. ???")
+OPTIONS=(1 "Auto-detection - [Default]"
+         2 "F16-RWKV-4-World-0.1B-Untuned       0.39GB / min. 1.5GB RAM"
+         3 "F32-RWKV-4-World-0.1B-Untuned       0.78GB / min. 3GB RAM"
+         4 "F16-RWKV-4-World-0.4B-Untuned       0.92GB / min. 4GB RAM"
+         5 "F32-RWKV-4-World-0.4B-Untuned       1.85GB / min. 6GB RAM"
+         6 "Q4_0-RWKV-4-World-1.5B-Untuned      1.19GB / min. 4GB RAM"
+         7 "Q4_0-RWKV-4-World-1.5B-CHNtuned     1.69GB / min. 6GB RAM"
+         8 "Q4_1-RWKV-4-World-1.5B-Untuned      1.26GB / min. 4GB RAM"
+         9 "Q4_1-RWKV-4-World-1.5B-CHNtuned     1.76GB / min. 6GB RAM"
+         10 "Q5_0-RWKV-4-World-1.5B-Untuned      1.34GB / min. 4/6GB RAM"
+         11 "Q5_0-RWKV-4-World-1.5B-CHNtuned     1.84GB / min. 6GB RAM"
+         12 "Q5_1-RWKV-4-World-1.5B-Untuned      1.42GB / min. 6GB RAM"
+         13 "Q5_1-RWKV-4-World-1.5B-CHNtuned     1.92GB / min. 6GB RAM"
+         14 "Q8_0-RWKV-4-World-1.5B-Untuned     1.8GB / min. 6GB RAM"
+         15 "Q8_0-RWKV-4-World-1.5B-CHNtuned    2.3GB / min. 8GB RAM"
+         16 "Q4_0-RWKV-4-World-3B-Untuned       2.06GB / min. 8GB RAM"
+         17 "Q4_0-RWKV-4-World-3B-CHNtuned      2.06GB / min. 8GB RAM"
+         18 "Q4_1-RWKV-4-World-3B-Untuned       2.22GB / min. 8GB RAM"
+         19 "Q4_1-RWKV-4-World-3B-CHNtuned      2.22GB / min. 8GB RAM"
+         20 "Q5_0-RWKV-4-World-3B-Untuned       2.37GB / min. 8GB RAM"
+         21 "Q5_0-RWKV-4-World-3B-CHNtuned      2.37GB / min. 8GB RAM"
+         22 "Q5_1-RWKV-4-World-3B-Untuned       2.53GB / min. 8/12GB RAM"
+         23 "Q5_1-RWKV-4-World-3B-CHNtuned      2.53GB / min. 8/12GB RAM"
+         24 "Q8_0-RWKV-4-World-3B-Untuned       3.33GB / min. 12GB RAM"
+         25 "Q8_0-RWKV-4-World-3B-CHNtuned      3.33GB / min. 12GB RAM"
+         26 "Q4_0-RWKV-4-World-7B-Untuned       4.66GB / min. 12GB RAM"
+         27 "Q4_1-RWKV-4-World-7B-Untuned       5.07GB / min. 12GB RAM"
+         28 "Q5_0-RWKV-4-World-7B-Untuned       5.47GB / min. 12GB RAM"
+         29 "Q5_1-RWKV-4-World-7B-Untuned       5.88GB / min. 12/16GB RAM"
+         30 "Q8_0-RWKV-4-World-7B-Untuned       7.91GB / min. 16GB RAM"
+         31 "Q4_0-open-llama-3b                1.93 GB / min. ???"
+         32 "Q4_1-open-llama-3b                2.14 GB / min. ???"
+         33 "Q5_0-open-llama-3b                2.36 GB / min. ???"
+         34 "Q5_1-open-llama-3b                2.57 GB / min. ???"
+         35 "Q8_0-open-llama-3b                3.64 GB / min. ???"
+         36 "F16-open-llama-3b                  6.85 GB / min. ???")
 
 
 CHOICE=$(dialog --clear \
@@ -55,6 +58,47 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
         1)
+            echo "You chose 'Auto-detection - [Default]'"
+	    echo "Downloading the AI model..."
+            cd '/data/data/com.termux/files/home/AltaeraAI-temp'
+            rm -rf 'model.bin'
+            
+   if [ $(free -h | awk '/Mem\:/ { print $2 }'
+) = "15Gi" ]; then
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-7B-v1-20230626-ctx4096.bin
+  mv 'q8_0-RWKV-4-World-7B-v1-20230626-ctx4096.bin' 'model.bin'
+	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
+) = "11Gi" ]; then
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-3B-v1-20230619-ctx4096.bin
+  mv 'q8_0-RWKV-4-World-3B-v1-20230619-ctx4096.bin' 'model.bin'
+	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
+) = "7Gi" ]; then
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q5_1-RWKV-4-World-3B-v1-20230619-ctx4096.bin
+  mv 'q5_1-RWKV-4-World-3B-v1-20230619-ctx4096.bin' 'model.bin'
+	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
+) = "5Gi" ]; then
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q5_1-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin
+  mv 'q5_1-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin' 'model.bin'
+	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
+) = "3Gi" ]; then
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q4_1-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin
+  mv 'q4_1-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin' 'model.bin'
+	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
+) = "2Gi" ]; then
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml/resolve/main/f32-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin
+  mv 'f32-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin' 'model.bin'
+	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
+) = "1Gi" ]; then
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml/resolve/main/f16-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin
+  mv 'f16-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin' 'model.bin'
+  else
+	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin
+  mv 'q8_0-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin' 'model.bin'
+fi
+            cd ..
+            clear
+	    ;;
+        2)
             echo "You chose 'F16-RWKV-4-World-0.1B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -64,7 +108,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        2)
+        3)
             echo "You chose 'F32-RWKV-4-World-0.1B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -74,7 +118,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        3)
+        4)
             echo "You chose 'F16-RWKV-4-World-0.4B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -84,7 +128,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        4)
+        5)
             echo "You chose 'F32-RWKV-4-World-0.4B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -94,7 +138,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        5)
+        6)
             echo "You chose 'Q4_0-RWKV-4-World-1.5B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -104,7 +148,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        6)
+        7)
             echo "你选择了 'Q4_0-RWKV-4-World-1.5B-CHNtuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -114,7 +158,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        7)
+        8)
             echo "You chose 'Q4_1-RWKV-4-World-1.5B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -124,7 +168,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        8)
+        9)
             echo "你选择了 'Q4_1-RWKV-4-World-1.5B-CHNtuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -134,7 +178,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        9)
+        10)
             echo "You chose 'Q5_0-RWKV-4-World-1.5B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -144,7 +188,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        10)
+        11)
             echo "你选择了 'Q5_0-RWKV-4-World-1.5B-CHNtuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -154,7 +198,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        11)
+        12)
             echo "You chose 'Q5_1-RWKV-4-World-1.5B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -164,7 +208,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        12)
+        13)
             echo "你选择了 'Q5_1-RWKV-4-World-1.5B-CHNtuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -174,7 +218,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        13)
+        14)
             echo "You chose 'Q8_0-RWKV-4-World-1.5B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -184,7 +228,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        14)
+        15)
             echo "你选择了 'Q8_0-RWKV-4-World-1.5B-CHNtuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -194,7 +238,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        15)
+        16)
             echo "You chose 'Q4_0-RWKV-4-World-3B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -204,7 +248,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        16)
+        17)
             echo "你选择了 'Q4_0-RWKV-4-World-3B-CHNtuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -214,7 +258,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        17)
+        18)
             echo "You chose 'Q4_1-RWKV-4-World-3B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -224,7 +268,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        18)
+        19)
             echo "你选择了 'Q4_1-RWKV-4-World-3B-CHNtuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -234,7 +278,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        19)
+        20)
             echo "You chose 'Q5_0-RWKV-4-World-3B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -244,7 +288,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        20)
+        21)
             echo "你选择了 'Q5_0-RWKV-4-World-3B-CHNtuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -254,7 +298,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        21)
+        22)
             echo "You chose 'Q5_1-RWKV-4-World-3B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -264,7 +308,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        22)
+        23)
             echo "你选择了 'Q5_1-RWKV-4-World-3B-CHNtuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -274,7 +318,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        23)
+        24)
             echo "You chose 'Q8_0-RWKV-4-World-3B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -284,7 +328,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        24)
+        25)
             echo "你选择了 'Q8_0-RWKV-4-World-3B-CHNtuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -294,7 +338,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        25)
+        26)
             echo "You chose 'Q4_0-RWKV-4-World-7B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -304,7 +348,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        26)
+        27)
             echo "You chose 'Q4_1-RWKV-4-World-7B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -314,7 +358,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        27)
+        28)
             echo "You chose 'Q5_0-RWKV-4-World-7B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -324,7 +368,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        28)
+        29)
             echo "You chose 'Q5_1-RWKV-4-World-7B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -334,7 +378,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        29)
+        30)
             echo "You chose 'Q8_0-RWKV-4-World-7B-Untuned'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -344,7 +388,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        30)
+        31)
             echo "You chose 'Q4_0-open-llama-3b'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -354,7 +398,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        31)
+        32)
             echo "You chose 'Q4_1-open-llama-3b'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -364,7 +408,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        32)
+        33)
             echo "You chose 'Q5_0-open-llama-3b'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -374,7 +418,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        33)
+        34)
             echo "You chose 'Q5_1-open-llama-3b'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -384,7 +428,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        34)
+        35)
             echo "You chose 'Q8_0-open-llama-3b'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
@@ -394,7 +438,7 @@ case $CHOICE in
             cd ..
             clear
             ;;
-        35)
+        36)
             echo "You chose 'F16-open-llama-3b'"
 	    echo "Downloading the AI model..."
             cd '/data/data/com.termux/files/home/AltaeraAI-temp'
