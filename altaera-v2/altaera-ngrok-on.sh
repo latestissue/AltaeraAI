56b0ee45ab8bf770aac13bc9d2d6e0c3a9df44b6
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-20 16:59:03 +0200

    Update altaera-ngrok-on.sh

--- altaera-v2/altaera-ngrok-on.sh
+++ altaera-v2/altaera-ngrok-on.sh
@@ -1,19 +1,6 @@
-#!/bin/bash
-
-dialog --title "ngrok configuration" \
-        --backtitle "AltaeraAI" \
-        --yesno "Are you sure you want to turn ON the ngrok Secure Tunnel?" 7 60
-
-response=$?
-case $response in
-   
-   0)   cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
-   rm -rf 'altaera.sh'
-   wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera-debian/ngrok/altaera.sh
-   chmod a+x 'altaera.sh'
-   cd '/data/data/com.termux/files/home'
-   rm -rf 'altaera-ngrok-on.sh'
-   ;;
-   1) ./altaera.sh;;
-   255) ./altaera.sh;;
-   esac
+cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
+rm -rf 'altaera.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera-debian/ngrok/altaera.sh
+chmod a+x 'altaera.sh'
+cd '/data/data/com.termux/files/home'
+rm -rf 'altaera-ngrok-on.sh'
