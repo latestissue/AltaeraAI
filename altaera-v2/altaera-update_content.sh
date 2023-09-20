cd6fb155718252cfa69603025461a45b5a9c272b
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-20 14:36:29 +0200

    Update altaera-update_content.sh

--- altaera-v2/altaera-update_content.sh
+++ altaera-v2/altaera-update_content.sh
@@ -61,10 +61,6 @@ rm -rf 'klite.embd'
 rm -rf 'altaera.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera-debian/altaera.sh
 chmod a+x 'altaera.sh'
-proot-distro login altaera
-apt update & apt upgrade -y
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
-logout
 cd '/data/data/com.termux/files/home'
 } &> /dev/null 2>&1;
 clear
