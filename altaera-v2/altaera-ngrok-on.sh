279933ee7e34ec50cf8af44e4ae9941abce5e20b
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-20 18:06:31 +0200

    Update altaera-ngrok-on.sh

--- altaera-v2/altaera-ngrok-on.sh
+++ altaera-v2/altaera-ngrok-on.sh
@@ -1,6 +1,1 @@
-cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
-rm -rf 'altaera.sh'
-wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera-debian/ngrok/altaera.sh
-chmod a+x 'altaera.sh'
-cd '/data/data/com.termux/files/home'
-rm -rf 'altaera-ngrok-on.sh'
+proot-distro login altaera -- ngrok http 1551
