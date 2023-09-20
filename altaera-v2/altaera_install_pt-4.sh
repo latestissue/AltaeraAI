13c3f9d7a12adf8498e70cf85e405c4cf0a3f079
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-20 14:29:28 +0200

    Update altaera_install_pt-4.sh

--- altaera-v2/altaera_install_pt-4.sh
+++ altaera-v2/altaera_install_pt-4.sh
@@ -8,6 +8,7 @@ rm -rf 'chat-with-bob.txt'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera-debian/prompts/chat-with-bob.txt
 cd /root
 apt install clang python3 libclblast-dev libopenblas-dev -y
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
 } &> /dev/null 2>&1;
 
 echo "Copying the AI model into proper directory...";
