f39e8757cd8b1d09685cbd4c34da1c72f5c68734
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-20 14:38:04 +0200

    Create altaera-upgrade.sh

---
+++ altaera-v2/altaera-debian/altaera-upgrade.sh
@@ -0,0 +1,5 @@
+apt update && apt upgrade -y
+
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
+
+logout
