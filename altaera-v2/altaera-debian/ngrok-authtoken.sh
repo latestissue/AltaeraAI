0d05b4a400e66191f262b0e4fbf71c2ec91fcf20
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-20 15:22:17 +0200

    Create ngrok-authtoken.sh

---
+++ altaera-v2/altaera-debian/ngrok-authtoken.sh
@@ -0,0 +1,8 @@
+user_input=$(\
+  dialog --title "ngrok authtoken configuration" \
+         --inputbox "Enter your ngrok authtoken:" 8 40 \
+  3>&1 1>&2 2>&3 3>&- \
+)
+
+ngrok config add-authtoken "$user_input"
+logout
