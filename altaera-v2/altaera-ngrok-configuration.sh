0adddb823b2b7959c440e7120d9f385d0cb02f8b
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-20 16:46:58 +0200

    Update altaera-ngrok-configuration.sh

--- altaera-v2/altaera-ngrok-configuration.sh
+++ altaera-v2/altaera-ngrok-configuration.sh
@@ -1,2 +1,14 @@
-proot-distro login altaera -- ./ngrok-authtoken.sh &
-exit
+#!/bin/bash
+
+dialog --title "ngrok configuration" \
+        --backtitle "AltaeraAI" \
+        --yesno "You will be forwarded to a website with an instruction on how to get your ngrok authtoken configured. Would you like to continue?" 7 60
+
+response=$?
+case $response in
+   
+   0)   termux-open-url ''
+   ;;
+   1) ./altaera.sh;;
+   255) ./altaera.sh;;
+   esac
