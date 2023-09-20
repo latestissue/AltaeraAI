1ef9146335cde85485d706ce703f209e5ce0e759
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-20 11:43:05 +0200

    Update altaera-model.sh

--- altaera-v2/altaera-model.sh
+++ altaera-v2/altaera-model.sh
@@ -29,24 +29,24 @@ case $CHOICE in
             
    if [ $(free -h | awk '/Mem\:/ { print $2 }'
 ) = "15Gi" ]; then
-	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-7B-v1-20230626-ctx4096.bin
-  mv 'q8_0-RWKV-4-World-7B-v1-20230626-ctx4096.bin' 'model.bin'
+	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5B-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin
+  mv 'q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin' 'model.bin'
 	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
 ) = "11Gi" ]; then
-	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-3B-v1-20230619-ctx4096.bin
-  mv 'q8_0-RWKV-4-World-3B-v1-20230619-ctx4096.bin' 'model.bin'
+	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5B-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin
+  mv 'q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin' 'model.bin'
 	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
 ) = "7Gi" ]; then
-	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q5_1-RWKV-4-World-3B-v1-20230619-ctx4096.bin
-  mv 'q5_1-RWKV-4-World-3B-v1-20230619-ctx4096.bin' 'model.bin'
+	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5B-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin
+  mv 'q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin' 'model.bin'
 	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
 ) = "5Gi" ]; then
-	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q5_1-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin
-  mv 'q5_1-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin' 'model.bin'
+	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5B-16k-ggml-quantized/resolve/main/q5_1-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin
+  mv 'q5_1-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin' 'model.bin'
 	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
 ) = "3Gi" ]; then
-	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q4_1-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin
-  mv 'q4_1-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin' 'model.bin'
+	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5B-16k-ggml-quantized/resolve/main/q4_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin
+  mv 'rwkv-claude-for-mobile-v4-world-1.5B-16k-ggml-quantized/resolve/main/q4_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin' 'model.bin'
 	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
 ) = "2Gi" ]; then
 	wget https://huggingface.co/latestissue/rwkv-4-world-ggml/resolve/main/f32-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin
@@ -56,8 +56,8 @@ case $CHOICE in
 	wget https://huggingface.co/latestissue/rwkv-4-world-ggml/resolve/main/f16-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin
   mv 'f16-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin' 'model.bin'
   else
-	wget https://huggingface.co/latestissue/rwkv-4-world-ggml-quantized/resolve/main/q8_0-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin
-  mv 'q8_0-RWKV-4-World-1.5B-v1-fixed-20230612-ctx4096.bin' 'model.bin'
+	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5B-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin
+  mv 'q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin' 'model.bin'
 fi
             cd /root
             clear
