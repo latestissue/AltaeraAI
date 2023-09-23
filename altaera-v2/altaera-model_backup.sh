ffda08c3e222cbd9d971b08f7044b3e30cd264e2
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-23 15:31:58 +0200

    Update altaera-model_backup.sh

--- altaera-v2/altaera-model_backup.sh
+++ altaera-v2/altaera-model_backup.sh
@@ -5,7 +5,9 @@ dialog --title "AltaeraAI - Model Backup" \
 
 response=$?
 case $response in
-   0)   mkdir '~/storage/shared/AltaeraAI-temp'
+   0)   cd '~/storage/shared'
+   mkdir 'AltaeraAI-temp'
+   cd '/data/data/com.termux/files/home'
    cp '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/koboldcpp-altaera/model.bin' '~/storage/AltaeraAI-temp'
     bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model_backed-up.sh';;
     
