f16f462d6b2a8bab0c321f3ceed614771c1231e8
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-23 16:44:51 +0200

    Update altaera_install_pt-4.sh

--- altaera-v2/altaera_install_pt-4.sh
+++ altaera-v2/altaera_install_pt-4.sh
@@ -15,7 +15,7 @@ echo "Copying the AI model into proper directory...";
 
 {
 cd '/root'
-cp '/data/data/com.termux/files/home/AltaeraAI-tmp/model.bin' '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/koboldcpp-altaera'
+mv '/data/data/com.termux/files/home/AltaeraAI-tmp/model.bin' '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/koboldcpp-altaera/model.bin'
 } &> /dev/null 2>&1;
 
 echo "Cleaning up File System...";
