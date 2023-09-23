5a45c845f08b11b6754d342039e6b699fcd2c5ab
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-23 15:59:03 +0200

    Update altaera-model_restored-cleanup.sh

--- altaera-v2/altaera-model_restored-cleanup.sh
+++ altaera-v2/altaera-model_restored-cleanup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 dialog --title "AltaeraAI - Model Restore [Cleanup]" \
 --backtitle "AltaeraAI" \
---yesno "Model succesfully restored! Would you also like to delete the backup from '/sdcard/AltaeraAI-temp/'?" 7 60
+--yesno "Model succesfully restored! Would you also like to delete the backup from '/sdcard/AltaeraAI-tmp/'?" 7 60
 
 response=$?
 case $response in
-   0)   rm -rf '/sdcard/AltaeraAI-temp/model.bin'
+   0)   rm -rf '/sdcard/AltaeraAI-tmp/model.bin'
    bash 'altaera-model_restored.sh';;
     
    1) bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh';;
