a522830c066e0d2b268c336e22ab96922f6264ee
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-23 13:17:25 +0200

    Create altaera-model_restored.sh

---
+++ altaera-v2/altaera-model_restored.sh
@@ -0,0 +1,5 @@
+dialog --backtitle "AltaeraAI" \
+--title "AltaeraAI - Model Restore" \
+--msgbox 'AI Model successfully restored!' 10 30
+
+bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
