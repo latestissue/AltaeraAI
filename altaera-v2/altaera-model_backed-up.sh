7713e7831513faca7f799cfc2e1b0ad7aee3869e
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-23 13:29:14 +0200

    Create altaera-model_backed-up.sh

---
+++ altaera-v2/altaera-model_backed-up.sh
@@ -0,0 +1,5 @@
+dialog --backtitle "AltaeraAI" \
+--title "AltaeraAI - Model Backup" \
+--msgbox 'AI Model successfully backed up!' 10 30
+
+bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
