45fbdb9aed2d910d8ce127959b97202247a2c74a
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-23 13:36:22 +0200

    Update and rename altaera-model_restored.sh to altaera-model_backup-cleaned-up.sh

--- altaera-v2/altaera-model_backup-cleaned-up.sh
+++ altaera-v2/altaera-model_backup-cleaned-up.sh
@@ -1,5 +1,5 @@
 dialog --backtitle "AltaeraAI" \
 --title "AltaeraAI - Model Restore" \
---msgbox 'AI Model successfully restored!' 10 30
+--msgbox 'AI Model Backup successfully deleted from '/sdcard/AltaeraAI-temp'. 10 30
 
 bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
