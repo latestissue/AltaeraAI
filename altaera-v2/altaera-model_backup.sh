9925727be1d690863579b6b3f7ca6bbac33c3c11
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-23 13:29:58 +0200

    Update altaera-model_backup.sh

--- altaera-v2/altaera-model_backup.sh
+++ altaera-v2/altaera-model_backup.sh
@@ -7,8 +7,9 @@ response=$?
 case $response in
    0)   mkdir '/sdcard/AltaeraAI-temp/'
     cp '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/koboldcpp-altaera/model.bin' '/sdcard/AltaeraAI-temp/';;
+    bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model_backed-up.sh'
     
-   1) bash 'altaera-model_backup-restore.sh';;
+   1) bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model_backup-restore.sh';;
    
-   255) bash 'altaera-model_backup-restore.sh';;
+   255) bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model_backup-restore.sh';;
 esac
