af3e6211ea9039b99f7d1b55b7fc757647b6c293
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-09-29 12:01:37 +0200

    Update altaera_reinstall_everything.sh

--- scripts/altaera_reinstall_everything.sh
+++ scripts/altaera_reinstall_everything.sh
@@ -12,15 +12,15 @@ case $response in
         rm -rf 'AltaeraAI'
         rm -rf 'AltaeraAI-temp'
         clear
-        wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera_install_pt-2.sh -q --show-progress
+        wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-2.sh -q --show-progress
         chmod a+x 'altaera_install_pt-2.sh'
         bash 'altaera_install_pt-2.sh'
         mkdir 'AltaeraAI-temp'
-        wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/altaera-v2/altaera_install_model.sh
+        wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_model.sh
         chmod a+x 'altaera_install_model.sh'
         bash 'altaera_install_model.sh'
         cd $PREFIX/etc/proot-distro
-        wget https://raw.githubusercontent.com/latestissue/altaera-packages.github.io/main/altaera-v2/altaera-debian/proot/altaera.sh -q --show-progress
+        wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-debian/proot/altaera.sh -q --show-progress
         proot-distro install altaera
         mv '/data/data/com.termux/files/home/AltaeraAI-temp/model.bin' '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/koboldcpp-altaera'
         rm -rf 'altaera_reinstall_everything.sh'
