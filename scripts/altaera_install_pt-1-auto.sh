333548fad6bc68d333dbce9a6ab74cda3ceaf28e
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-01 15:39:25 +0200

    Update altaera_install_pt-1-auto.sh

--- scripts/altaera_install_pt-1-auto.sh
+++ scripts/altaera_install_pt-1-auto.sh
@@ -8,23 +8,23 @@ echo "Downloading the AI model..."
 
    if [ $(free -h | awk '/Mem\:/ { print $2 }'
 ) = "15Gi" ]; then
-	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5B-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -q --show-progress
+	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5b-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -q --show-progress
   mv 'q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin' 'model.bin'
 	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
 ) = "11Gi" ]; then
-	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5B-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -q --show-progress
+	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5b-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -q --show-progress
   mv 'q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin' 'model.bin'
 	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
 ) = "7Gi" ]; then
-	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5B-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -q --show-progress
+	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5b-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -q --show-progress
   mv 'q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin' 'model.bin'
 	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
 ) = "5Gi" ]; then
-	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5B-16k-ggml-quantized/resolve/main/q5_1-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -q --show-progress
+	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5b-16k-ggml-quantized/resolve/main/q5_1-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -q --show-progress
   mv 'q5_1-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin' 'model.bin'
 	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
 ) = "3Gi" ]; then
-	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5B-16k-ggml-quantized/resolve/main/q4_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -q --show-progress
+	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5b-16k-ggml-quantized/resolve/main/q4_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -q --show-progress
   mv 'q4_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin' 'model.bin'
 	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
 ) = "2Gi" ]; then
@@ -35,7 +35,7 @@ echo "Downloading the AI model..."
 	wget https://huggingface.co/latestissue/rwkv-4-world-ggml/resolve/main/f16-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin -q --show-progress
   mv 'f16-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin' 'model.bin'
   else
-	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5B-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -q --show-progress
+	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5b-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -q --show-progress
   mv 'q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin' 'model.bin'
 fi
 
