57ab44763ac1d67054fd075cd7aa2999ebb31edb
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-03 11:58:19 +0200

    Create altaera_install_model-chn.sh

---
+++ scripts/altaera_install_model-chn.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+HEIGHT=30
+WIDTH=60
+CHOICE_HEIGHT=7
+BACKTITLE="AltaeraAI"
+TITLE="安装 AltaeraAI - 人工智能模型"
+MENU="选择所需的方法："
+
+OPTIONS=(1 "人工智能模型自动检测 (RWKV-4-Claude) [默认值]"
+         2 "人工智能模型手册选择"
+         3 "请勿下载人工智能模型 (?) - [...]")
+
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            rm -rf 'altaera_install_pt-1.sh'
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-1-auto.sh -O 'altaera_install_pt-1.sh'
+            chmod a+x 'altaera_install_pt-1.sh'
+            bash altaera_install_pt-1.sh
+            ;;
+        2)
+            rm -rf 'altaera_install_pt-1.sh'
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-1-manual.sh -O 'altaera_install_pt-1.sh'
+            chmod a+x 'altaera_install_pt-1.sh'
+            bash altaera_install_pt-1.sh
+            ;;
+        3)
+            rm -rf 'altaera_install_pt-1.sh'
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-1-no-model.sh -O 'altaera_install_pt-1.sh'
+            chmod a+x 'altaera_install_pt-1.sh'
+            bash altaera_install_pt-1.sh
+            ;;
+esac
