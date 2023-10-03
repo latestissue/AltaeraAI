17a910e367797169142fea8ecb8b836d6c8fee90
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-03 14:35:05 +0200

    Create altaera_install_pt-2-pol.sh

---
+++ scripts/altaera_install_pt-2-pol.sh
@@ -0,0 +1,60 @@
+clear
+echo "Installing initial files...";
+
+{
+cd /data/data/com.termux/files/home
+rm -rf 'altaera_install.sh'
+mkdir 'AltaeraAI'
+cd 'AltaeraAI'
+
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-reinstall.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-uninstall.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_pol.sh -O 'altaera.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaeralogin.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-lang.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model-rwkv-4.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_model.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-ngrok.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-ngrok-configuration.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-ngrok-on.sh
+
+chmod a+x 'altaera-update.sh'
+chmod a+x 'altaera-reinstall.sh'
+chmod a+x 'altaera-uninstall.sh'
+chmod a+x 'altaera.sh'
+chmod a+x 'altaeralogin.sh'
+chmod a+x 'altaera-lang.sh'
+chmod a+x 'altaera-model.sh'
+chmod a+x 'altaera-model-rwkv-4.sh'
+chmod a+x 'altaera_install_model.sh'
+chmod a+x 'altaera-ngrok.sh'
+chmod a+x 'altaera-ngrok-configuration.sh'
+chmod a+x 'altaera-ngrok-on.sh'
+
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model_backup-restore.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model_backup.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model_restore.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model_backed-up.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model_restored-cleanup.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model_backup-cleaned-up.sh
+
+chmod a+x 'altaera-model_backup-restore.sh'
+chmod a+x 'altaera-model_backup.sh'
+chmod a+x 'altaera-model_restore.sh'
+chmod a+x 'altaera-model_backed-up.sh'
+chmod a+x 'altaera-model_restored-cleanup.sh'
+chmod a+x 'altaera-model_backup-cleaned-up.sh'
+
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/bash.bashrc
+
+rm -rf '/data/data/com.termux/files/usr/etc/bash.rc'
+cp 'bash.bashrc' '/data/data/com.termux/files/usr/etc/'
+rm -rf 'bash.bashrc'
+rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/initial.sh'
+rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/bash.bashrc'
+rm -rf 'initial.sh'
+rm -rf 'bash.bashrc'
+cd ..
+} &> /dev/null 2>&1;
