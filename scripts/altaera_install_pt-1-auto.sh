f956e286e65fa720af94f39fe405cf115c549ec5
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-04 11:31:00 +0200

    Update altaera_install_pt-1-auto.sh

--- scripts/altaera_install_pt-1-auto.sh
+++ scripts/altaera_install_pt-1-auto.sh
@@ -12,6 +12,7 @@ echo "Downloading the AI model..."
   mv 'q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin' 'model.bin'
 	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
 ) = "11Gi" ]; then
+	clear
 	wget https://huggingface.co/latestissue/rwkv-claude-for-mobile-v4-world-1.5b-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -q --show-progress
   mv 'q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin' 'model.bin'
 	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
