24604f57ad2c6dd848b4ad108be74412c103b170
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-05 14:18:26 +0200

    Update altaera_install_pt-4.sh

--- scripts/altaera_install_pt-4.sh
+++ scripts/altaera_install_pt-4.sh
@@ -14,11 +14,6 @@ echo "Cleaning up File System...";
 rm -rf 'chat-with-bob.txt'
 rm -rf 'altaera_install_pt-3.sh'
 rm -rf 'altaera_install_pt-4.sh'
-rm -rf '/data/data/com.termux/files/home/install'
-rm -rf '/data/data/com.termux/files/home/altaera_install_pt-1.sh'
-rm -rf '/data/data/com.termux/files/home/altaera_install_pt-2.sh'
-rm -rf '/data/data/com.termux/files/home/altaera-update_content.sh'
-rm -rf '/data/data/com.termux/files/home/altaera_install_model.sh'
 } &> /dev/null 2>&1;
 
 clear
