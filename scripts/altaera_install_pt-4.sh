a5548faddb1ef7d2c5a6138025595749ed3a7766
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-05 12:30:41 +0200

    Update altaera_install_pt-4.sh

--- scripts/altaera_install_pt-4.sh
+++ scripts/altaera_install_pt-4.sh
@@ -8,14 +8,6 @@ apt install clang python3 libclblast-dev libopenblas-dev -y
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
 } &> /dev/null 2>&1;
 
-echo "Moving the AI model and prompt file into proper directory...";
-
-{
-cd '/root'
-mv '/data/data/com.termux/files/home/AltaeraAI-tmp/chat-with-bob.txt' '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/koboldcpp-altaera/prompts/chat-with-bob.txt'
-mv '/data/data/com.termux/files/home/AltaeraAI-tmp/model.bin' '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/koboldcpp-altaera/model.bin'
-} &> /dev/null 2>&1;
-
 echo "Cleaning up File System...";
 
 {
@@ -30,24 +22,3 @@ rm -rf '/data/data/com.termux/files/home/altaera_install_model.sh'
 } &> /dev/null 2>&1;
 
 clear
-echo "
-
-              **********INSTALLATION COMPLETED**********
-_______________________________________________________________________
-
-
-      !!!  Please pull down the notification bar, expand 'Termux' and press 'Exit'  !!!
-
-      
-      --- Then, open Termux and:
-
-
-- To start, please type in 'ae'.
-
-
-_______________________________________________________________________
-
-(IGNORE the text on the bottom)
-              |
-              v
-              "
