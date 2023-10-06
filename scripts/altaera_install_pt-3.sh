ced91ecdd7834fc02aee1e3cb36c98a05873318e
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-06 11:12:22 +0200

    Update altaera_install_pt-3.sh

--- scripts/altaera_install_pt-3.sh
+++ scripts/altaera_install_pt-3.sh
@@ -14,7 +14,7 @@ tar -xf 'altaera-v2.7.tar.gz'
 rm -rf 'altaera-v2.7.tar.gz'
 cd 'koboldcpp-altaera'
 rm -rf 'klite.embd'
-wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-debian/klite/klite.embd
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-ubuntu/klite/klite.embd
 cd /root
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-4.sh
 chmod a+x 'altaera_install_pt-4.sh'
