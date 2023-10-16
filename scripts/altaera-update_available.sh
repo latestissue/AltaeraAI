b870c413c0f921325f7813ca6c6a2d2d2db791c2
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-16 11:58:37 +0200

    Create altaera-update_available.sh

---
+++ scripts/altaera-update_available.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+dialog --title "AltaeraAI - Check for updates" \
+--backtitle "AltaeraAI" \
+--yesno "There is an update available. Would you like to update now?'?" 7 60
+
+response=$?
+case $response in
+   0)   bash 'AltaeraAI/altaera-update.sh'
+    
+   1) bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh';;
+   
+   255) bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh';;
+esac
