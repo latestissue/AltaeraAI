a8ad25e81966746c7c50a0ba2e0bf6e64b7d0ae5
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-16 12:36:36 +0200

    Update altaera-check_for_updates.sh

--- scripts/altaera-check_for_updates.sh
+++ scripts/altaera-check_for_updates.sh
@@ -7,22 +7,20 @@ dialog --title "Check for updates" \
 response=$?
 case $response in
 
-clear
-case $CHOICE in
-0) 
-cd "AltaeraAI"
-wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-version_upstream.sh
-chmod a+x 'altaera-version_upstream.sh'
-cd ..
+        0) 
+        cd "AltaeraAI"
+        wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-version_upstream.sh
+        chmod a+x 'altaera-version_upstream.sh'
+        cd ..
  
- if [ $(bash 'AltaeraAI/altaera-version_upstream.sh' | awk '/Mem\:/ { print $2 }'
-  ) = "v2.8" ]; then
-  rm -rf 'AltaeraAI/altaera-version_upstream.sh'
-  bash 'AltaeraAI/altaera-up-to-date.sh'
-  else
-  rm -rf 'AltaeraAI/altaera-version_upstream.sh'
-  bash 'AltaeraAI/altaera-update_available.sh'
-  ;;
-     1) ./data/data/com.termux/files/home/AltaeraAI/altaera.sh;;
-   255) ./data/data/com.termux/files/home/AltaeraAI/altaera.sh;;
+         if [ $(bash 'AltaeraAI/altaera-version_upstream.sh' | awk '/Mem\:/ { print $2 }'
+          ) = "v2.8" ]; then
+          rm -rf 'AltaeraAI/altaera-version_upstream.sh'
+          bash 'AltaeraAI/altaera-up-to-date.sh'
+          else
+          rm -rf 'AltaeraAI/altaera-version_upstream.sh'
+          bash 'AltaeraAI/altaera-update_available.sh'
+          ;;
+        1) ./data/data/com.termux/files/home/AltaeraAI/altaera.sh;;
+        255) ./data/data/com.termux/files/home/AltaeraAI/altaera.sh;;
 esac
