a6f5a667c810ff0e4d754d44c244e6fb3c679e6f
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-16 12:10:45 +0200

    Update altaera-check_for_updates.sh

--- scripts/altaera-check_for_updates.sh
+++ scripts/altaera-check_for_updates.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+cd "AltaeraAI"
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-version_upstream.sh
+chmod a+x 'altaera-version_upstream.sh'
+cd ..
  
- if [ $(bash 'AltaeraAI/version.sh' | awk '/Mem\:/ { print $2 }'
+ if [ $(bash 'AltaeraAI/altaera-version_upstream.sh' | awk '/Mem\:/ { print $2 }'
   ) = "v2.8" ]; then
+  rm -rf 'AltaeraAI/altaera-version_upstream.sh'
   bash 'AltaeraAI/altaera-up-to-date.sh'
   else
+  rm -rf 'AltaeraAI/altaera-version_upstream.sh'
   bash 'AltaeraAI/altaera-update_available.sh'
