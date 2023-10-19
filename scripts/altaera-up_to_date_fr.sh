d1a650b0ea8f13845b3ca0066b97a4b247aaa52d
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-19 13:47:58 +0200

    Create altaera-up_to_date_fr.sh

---
+++ scripts/altaera-up_to_date_fr.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+dialog --backtitle "AltaeraAI" \
+--title "AltaeraAI - Vérification des mises à jour" \
+--msgbox 'Votre AltaeraAI est à jour !' 10 30
+
+bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
