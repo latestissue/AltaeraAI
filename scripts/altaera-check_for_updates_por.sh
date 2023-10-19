b9e282f9be98bf659e2f98db54976d85d40d5751
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-19 12:03:12 +0200

    Create altaera-check_for_updates_por.sh

---
+++ scripts/altaera-check_for_updates_por.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+dialog --title "Verificação de actualizações" \
+        --backtitle "AltaeraAI" \
+        --yesno "Gostaria de verificar se há actualizações?" 7 60
+
+response=$?
+case $response in
+
+        0)      
+        echo "Verificar a disponibilidade de actualizações...";
+        
+                {
+                cd "AltaeraAI"
+                wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-version_upstream.sh
+                chmod a+x 'altaera-version_upstream.sh'
+                cd ..
+                } &> /dev/null 2>&1;
+ 
+         if [ $(bash 'AltaeraAI/altaera-version_upstream.sh'
+          ) = "v2.8.1" ]; then
+          rm -rf 'AltaeraAI/altaera-version_upstream.sh'
+          bash 'AltaeraAI/altaera-up_to_date.sh'
+          else
+          rm -rf 'AltaeraAI/altaera-version_upstream.sh'
+          bash 'AltaeraAI/altaera-update_available.sh'
+        fi
+          ;;
+        1) ./data/data/com.termux/files/home/AltaeraAI/altaera.sh;;
+        255) ./data/data/com.termux/files/home/AltaeraAI/altaera.sh;;
+esac
