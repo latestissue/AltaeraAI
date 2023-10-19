f0c81bd377476a51e241dd19a0f83a873e638f67
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-19 11:16:24 +0200

    Create altaera-update_por.sh

---
+++ scripts/altaera-update_por.sh
@@ -0,0 +1,10 @@
+echo "Updating AltaeraAI... Please do not exit Termux!";
+
+{
+rm -rf 'altaera-update_content.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content_por.sh -O 'altaera-update_content.sh'
+chmod a+x 'altaera-update_content.sh'
+bash 'altaera-update_content.sh'
+} &> /dev/null 2>&1;
+clear
+bash 'AltaeraAI/altaera-updated_successfully.sh'
