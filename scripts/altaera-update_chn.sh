7e0f4c0d118d1fc8cd45148ef197cf3f4755379b
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-19 11:13:03 +0200

    Update altaera-update_chn.sh

--- scripts/altaera-update_chn.sh
+++ scripts/altaera-update_chn.sh
@@ -2,7 +2,7 @@ echo "Updating AltaeraAI... Please do not exit Termux!";
 
 {
 rm -rf 'altaera-update_content.sh'
-wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content_chn.sh -O 'altaera-update_content_chn.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content_chn.sh -O 'altaera-update_content.sh'
 chmod a+x 'altaera-update_content.sh'
 bash 'altaera-update_content.sh'
 } &> /dev/null 2>&1;
