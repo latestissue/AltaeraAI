71d812694f6e109152aa969d4d29af72874c863e
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-19 11:48:15 +0200

    Create altaera-check_for_updates_chn.sh

---
+++ scripts/altaera-check_for_updates_chn.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+dialog --title "检查更新的可用性" \
+        --backtitle "AltaeraAI" \
+        --yesno "您想检查更新吗？" 7 60
+
+response=$?
+case $response in
+
+        0)      
+        echo "检查更新的可用性...";
+        
+                {
+                cd "AltaeraAI"
+                wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-version_upstream.sh
+                chmod a+x 'altaera-version_upstream.sh'
+                cd ..
+                } &> /dev/null 2>&1;
+ 
+         if [ $(bash 'AltaeraAI/altaera-version_upstream.sh'
+          ) = "v2.8.1" ]; then
+          rm -rf 'AltaeraAI/altaera-version_upstream.sh'
+          bash 'AltaeraAI/altaera-up_to_date.sh'
+          else
+          rm -rf 'AltaeraAI/altaera-version_upstream.sh'
+          bash 'AltaeraAI/altaera-update_available.sh'
+        fi
+          ;;
+        1) ./data/data/com.termux/files/home/AltaeraAI/altaera.sh;;
+        255) ./data/data/com.termux/files/home/AltaeraAI/altaera.sh;;
+esac
