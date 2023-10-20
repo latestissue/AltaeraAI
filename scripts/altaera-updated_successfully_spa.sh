7c8b178afcdecfaed2430be958f964701aa1bbc1
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-20 11:52:32 +0200

    Create altaera-updated_successfully_spa.sh

---
+++ scripts/altaera-updated_successfully_spa.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+dialog --backtitle "AltaeraAI" \
+--title "AltaeraAI - Actualizando" \
+--msgbox '¡AltaeraAI ha sido actualizado con éxito!' 10 30
+
+bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
