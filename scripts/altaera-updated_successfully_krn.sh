f8d9334f87e02b15e859e399c75ae2be497b2ddb
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-20 11:46:38 +0200

    Create altaera-updated_successfully_krn.sh

---
+++ scripts/altaera-updated_successfully_krn.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+dialog --backtitle "AltaeraAI" \
+--title "AltaeraAI - 업데이트" \
+--msgbox '알테라AI가 성공적으로 업데이트되었습니다!' 10 30
+
+bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
