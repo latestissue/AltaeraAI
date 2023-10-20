eac8470b0134364435bc53c0b66345070b62a15e
Krystian Paweł Węgrzyniak <121747280+latestissue@users.noreply.github.com>
2023-10-20 12:38:46 +0200

    Update altaera_install_pt-2-krn.sh

--- scripts/altaera_install_pt-2-krn.sh
+++ scripts/altaera_install_pt-2-krn.sh
@@ -13,9 +13,9 @@ mkdir 'AltaeraAI'
 cd 'AltaeraAI'
 
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-check_for_updates_krn.sh -O 'altaera-check_for_updates.sh'
-wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-up_to_date.sh
-wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_available.sh
-wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-updated_successfully.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-up_to_date_krn.sh -O 'altaera-up_to_date.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_available_krn.sh -O 'altaera-update_available.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-updated_successfully_krn.sh -O 'altaera-updated_successfully.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_krn.sh -O 'altaera-update.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-force_update_krn.sh -O 'altaera-force_update.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-reinstall.sh
