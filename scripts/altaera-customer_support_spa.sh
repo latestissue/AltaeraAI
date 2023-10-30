152d62898a2ebe97d236efd6a46b7ba40a060c3f
Krystian Paweł Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-10-30 13:42:42 +0100

    Create altaera-customer_support_spa.sh

---
+++ scripts/altaera-customer_support_spa.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+HEIGHT=30
+WIDTH=60
+CHOICE_HEIGHT=7
+BACKTITLE="AltaeraAI"
+TITLE="Atención al cliente"
+MENU="¿Qué necesita?"
+
+OPTIONS=(1 "Política de privacidad [URL]"
+         2 "Asistencia técnica [URL]"
+         3 "Contacto [URL]"
+         4 "FAQ [URL] - ...en creación")
+         
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            clear
+            termux-open-url 'https://altaera.ai/privacy-policy/'
+            ;;
+        2)
+            clear
+            termux-open-url 'https://altaera.ai/technical-support/'
+            ;;
+        3)
+            clear
+            termux-open-url 'https://altaera.ai/contact/'
+            ;;
+        4)
+            clear
+            termux-open-url 'https://altaera.ai/faq/'
+            ;;
+esac
