588adf772c3f40873e0b412eebd57b5981a3f9e6
Krystian Paweł Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-10-31 14:34:21 +0100

    Create altaera-koboldcpp_settings_property_changed_successfully.sh

---
+++ scripts/altaera-koboldcpp_settings_property_changed_successfully.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+dialog --backtitle "AltaeraAI" \
+--title "AltaeraAI - KoboldCpp Settings" \
+--msgbox 'The property was changed successfully!' 10 30
+
+bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
