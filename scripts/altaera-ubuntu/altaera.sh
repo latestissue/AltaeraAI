d3841d51b3108f609be43e4f537e551bf64d2ef4
Krystian Paweł Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-10-31 13:30:55 +0100

    Update altaera.sh

--- scripts/altaera-ubuntu/altaera.sh
+++ scripts/altaera-ubuntu/altaera.sh
@@ -2,4 +2,7 @@
 
 cd "koboldcpp-altaera"
 termux-open-url 'http://localhost:1551'
-python3 koboldcpp.py model.bin 1551 --smartcontext --blasbatchsize 2048 --contextsize 2048
+python3 koboldcpp.py model.bin 1551
+--smartcontext
+--blasbatchsize 2048
+--contextsize 2048
