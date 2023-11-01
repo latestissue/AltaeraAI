7c6d05ca0174ece29282d8204effb69fce70f795
Krystian Paweł Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-01 14:28:06 +0100

    Update altaera-up_to_date.sh

--- scripts/altaera-up_to_date.sh
+++ scripts/altaera-up_to_date.sh
@@ -4,4 +4,4 @@ dialog --backtitle "AltaeraAI" \
 --title "AltaeraAI - Check for updates" \
 --msgbox 'Your AltaeraAI is up to date!' 10 30
 
-bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
+exit
