45597dfc5e2cf976a9fe356ab4a6b99f38d4cfa5
Krystian Paweł Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-10 13:53:22 +0100

    Update altaera-update_content.sh

--- scripts/altaera-update_content.sh
+++ scripts/altaera-update_content.sh
@@ -112,8 +112,8 @@ rm -rf 'upgrade.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-ubuntu/upgrade.sh
 chmod a+x 'upgrade.sh'
 rm -rf 'ngrok-authtoken.sh'
-wget https://github.com/latestissue/AltaeraAI/releases/download/v3.0/altaera-v3.0.tar.gz
-tar -xf 'altaera-v3.0.tar.gz'
+wget https://github.com/latestissue/AltaeraAI/releases/download/v2.9/altaera-v2.9.tar.gz
+tar -xf 'altaera-v2.9.tar.gz'
 cd 'koboldcpp-altaera'
 rm -rf 'klite.embd'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-ubuntu/klite/klite.embd
@@ -130,7 +130,6 @@ rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/al
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.7.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.8.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.9.tar.gz'
-rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v3.0.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-temporary'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/chat-with-bob.txt'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera_install_pt-3.sh'
