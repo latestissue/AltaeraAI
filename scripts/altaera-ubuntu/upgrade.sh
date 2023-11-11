5455c814cc1f11c05075aa6b0e018c3fd6bd35fa
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-11 16:38:36 +0100

    Update upgrade.sh

--- scripts/altaera-ubuntu/upgrade.sh
+++ scripts/altaera-ubuntu/upgrade.sh
@@ -1,4 +1,4 @@
-yes | pacman -Syu
+pacman -Syu --noconfirm
 
 ### curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
 
