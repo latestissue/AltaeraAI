fe55ffa1f8c3c9be34eb5b760a701818f97d03cc
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-11 15:15:40 +0100

    Update altaera_install_pt-4.sh

--- scripts/altaera_install_pt-4.sh
+++ scripts/altaera_install_pt-4.sh
@@ -4,7 +4,7 @@ echo "Installing dependencies...";
 
 {
 cd '/root'
-pacman -S clang python clblast openblas -y
+yes | pacman -S clang python clblast openblas -y
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
 } &> /dev/null 2>&1;
 
