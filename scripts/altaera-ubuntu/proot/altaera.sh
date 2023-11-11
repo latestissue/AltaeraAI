35855b1945614026069264eb052127ac0ffcfa39
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-11 15:59:50 +0100

    Update altaera.sh

--- scripts/altaera-ubuntu/proot/altaera.sh
+++ scripts/altaera-ubuntu/proot/altaera.sh
@@ -5,7 +5,7 @@ TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download
 TARBALL_SHA256['aarch64']="9801031864da6bc9dc69293695797f8aba7866c273bb7403f95c4e70be987936"
 
 distro_setup() {
-	run_proot_cmd yes | pacman -Syyu
+	run_proot_cmd pacman -Syyu
 	run_proot_cmd yes | pacman -S curl wget procps
 	run_proot_cmd wget --no-check-certificate https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-3.sh
  	run_proot_cmd chmod a+x 'altaera_install_pt-3.sh'
