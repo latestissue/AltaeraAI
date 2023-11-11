966e174f75da05aae9f12f9153ffd9cf23ff5765
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-11 15:00:20 +0100

    Update altaera.sh

--- scripts/altaera-ubuntu/proot/altaera.sh
+++ scripts/altaera-ubuntu/proot/altaera.sh
@@ -1,5 +1,5 @@
-DISTRO_NAME="Fedora_AltaeraAI"
-DISTRO_COMMENT="Fedora 38 utilised by the AltaeraAI project"
+DISTRO_NAME="Artix_AltaeraAI"
+DISTRO_COMMENT="Artix Linux utilised by the AltaeraAI project"
 
 TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.18.1/artix-aarch64-pd-v3.18.1.tar.xz"
 TARBALL_SHA256['aarch64']="9801031864da6bc9dc69293695797f8aba7866c273bb7403f95c4e70be987936"
