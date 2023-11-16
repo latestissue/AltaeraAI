1f77395765d2686cf80d4058256f39c3c18fe892
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-16 17:45:07 +0100

    Update altaera.sh

--- scripts/altaera-artix/altaera.sh
+++ scripts/altaera-artix/altaera.sh
@@ -11,7 +11,7 @@ if [ ${#files[@]} -eq 0 ]; then
     clear
     echo "No .bin files found in $directory"
 else
-    file=$(dialog --stdout --title "Select an AI Model:" --cancel-label "Back" --menu "Choose a file:" 0 0 0 "${files[@]}")
+    file=$(dialog --stdout --title "Select an AI Model:" --menu "Choose a file:" 0 0 0 "${files[@]}")
 fi
 
 RESULT=$?
