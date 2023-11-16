c1b0588623038c48dc69e47f223bd47e074e4982
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-16 17:44:19 +0100

    Update altaera.sh

--- scripts/altaera-artix/altaera.sh
+++ scripts/altaera-artix/altaera.sh
@@ -4,14 +4,14 @@ files=() #blank the variable so its empty for next use
 # Loop folder, add files to array
 while IFS= read -r -d $'\0' file; do
     files+=("$file" "")
-done < <(find "/root/koboldcpp-altaera" -maxdepth 1 -type f -name "*.b>
+done < <(find "/root/koboldcpp-altaera" -maxdepth 1 -type f -name "*.bin" -print0)
 # or for all files: done < <(find "/home" -maxdepth 1 -type f -print0)
 # Check it has at least 1 file to show (otherwise dialog errors)
 if [ ${#files[@]} -eq 0 ]; then
     clear
     echo "No .bin files found in $directory"
 else
-    file=$(dialog --stdout --title "Select an AI Model:" --menu "Choose a file:" 0 0 0 "${files[@]}")
+    file=$(dialog --stdout --title "Select an AI Model:" --cancel-label "Back" --menu "Choose a file:" 0 0 0 "${files[@]}")
 fi
 
 RESULT=$?
