cb03adfc410764b15c3d5a26f0be64848cddb553
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-17 15:31:16 +0100

    Update altaera.sh

--- scripts/altaera-artix/proot/altaera.sh
+++ scripts/altaera-artix/proot/altaera.sh
@@ -1,12 +1,12 @@
 DISTRO_NAME="Artix_AltaeraAI"
 DISTRO_COMMENT="Artix Linux utilised by the AltaeraAI project"
 
-TARBALL_URL['aarch64']="https://github.com/latestissue/AltaeraAI/releases/download/artix-altaera-v1.0/artix-altaera-aarch64-pd-v1.0.tar.xz"
-TARBALL_SHA256['aarch64']="520a29b67d434d2a8814b9feed7a793c406cd5ada8602a193f9efe8fa0429807"
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.18.1/artix-aarch64-pd-v3.18.1.tar.xz"
+TARBALL_SHA256['aarch64']="9801031864da6bc9dc69293695797f8aba7866c273bb7403f95c4e70be987936"
 
 distro_setup() {
 	run_proot_cmd pacman -Syu --noconfirm
-##	run_proot_cmd pacman -S curl wget procps --noconfirm	### [No longer needed since v3.1 due to own tarball file]
+	run_proot_cmd pacman -S curl wget procps --noconfirm
 	run_proot_cmd wget --no-check-certificate https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-3.sh
  	run_proot_cmd chmod a+x 'altaera_install_pt-3.sh'
  	run_proot_cmd bash 'altaera_install_pt-3.sh'
