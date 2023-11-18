93e8cbeded39772d43c45cd28a3615a9c9b1536a
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-18 15:18:28 +0100

    Update altaera-model_remove.sh

--- scripts/altaera-model_remove.sh
+++ scripts/altaera-model_remove.sh
@@ -6,7 +6,7 @@ files=() #blank the variable so its empty for next use
 # Loop folder, add files to array
 while IFS= read -r -d $'\0' file; do
     files+=("$file" "")
-done < <(find "/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/koboldcpp-altaera" -maxdepth 1 -type f -name "*.bin" -print0)
+done < <(find "/root/koboldcpp-altaera" -maxdepth 1 -type f -name "*.bin" -print0)
 # or for all files: done < <(find "/home" -maxdepth 1 -type f -print0)
 # Check it has at least 1 file to show (otherwise dialog errors)
 if [ ${#files[@]} -eq 0 ]; then
@@ -19,7 +19,7 @@ fi
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
 
-    cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/koboldcpp-altaera'
+    cd '/root/koboldcpp-altaera'
     rm -rf $file
 
     echo  "
