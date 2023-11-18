716008c796efb4a32975609daf963d62865444bd
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-18 13:45:03 +0100

    Create altaera-manage_models.sh

---
+++ scripts/altaera-manage_models.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+HEIGHT=20
+WIDTH=60
+CHOICE_HEIGHT=24
+BACKTITLE="AltaeraAI - AI Model Settings"
+TITLE="AI Model Selection"
+MENU="What would you like to do?:"
+
+OPTIONS=(1 "[...] Go Back"
+         2 "Download Models"
+         3 "Remove Models")
+
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            exit
+	    ;;
+        2)
+            clear
+            './AltaeraAI/altaera-model.sh'
+            './AltaeraAI/altaera.sh'
+            ;;
+        3)
+            clear
+            './AltaeraAI/altaera-model_remove.sh'
+            './AltaeraAI/altaera.sh'
+            ;;
+esac
