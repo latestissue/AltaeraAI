4f180a8ed373b2f0c849f5d478bbc6f109be07b3
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-19 11:57:03 +0100

    Delete scripts/altaera-model_backed-up.sh

--- scripts/altaera-model_backed-up.sh
+++
@@ -1,5 +0,0 @@
-dialog --backtitle "AltaeraAI" \
---title "AltaeraAI - Model Backup" \
---msgbox 'AI Model successfully backed up!' 10 30
-
-bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
