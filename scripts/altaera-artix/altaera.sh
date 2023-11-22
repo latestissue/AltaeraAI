1a8fe5f8fbe8280bccf1ab0a0710f657677e885f
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-22 13:57:11 +0100

    Update altaera.sh

--- scripts/altaera-artix/altaera.sh
+++ scripts/altaera-artix/altaera.sh
@@ -4,12 +4,12 @@ files=() #blank the variable so its empty for next use
 # Loop folder, add files to array
 while IFS= read -r -d $'\0' file; do
     files+=("$file" "")
-done < <(find "/root/kcpp-ae/models" -maxdepth 1 -type f -name "*.bin" -print0)
+done < <(find "/root/kcpp-ae/models" -maxdepth 1 -type f -name "*.bin" "*.gguf" -print0)
 # or for all files: done < <(find "/home" -maxdepth 1 -type f -print0)
 # Check it has at least 1 file to show (otherwise dialog errors)
 if [ ${#files[@]} -eq 0 ]; then
     clear
-    echo "No .bin files found in $directory"
+    echo "No .bin or .gguf files found in $directory"
 else
     file=$(dialog --stdout --title "Select an AI Model:" --menu "Choose a file:" 0 0 0 "${files[@]}")
 fi
