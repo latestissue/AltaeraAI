aa63b848c00d5b642cb93cb5c53473a8c659afc1
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-23 12:01:55 +0100

    Update altaera_install_model.sh

--- scripts/altaera_install_model.sh
+++ scripts/altaera_install_model.sh
@@ -7,9 +7,10 @@ BACKTITLE="AltaeraAI"
 TITLE="Installing AltaeraAI - AI Model"
 MENU="Choose your desired method:"
 
-OPTIONS=(1 "AI Model Auto-Detection (RWKV-4-Claude) [Default]"
-         2 "AI Model Manual Selection"
-         3 "Do not download AI Model (?) - [...]")
+OPTIONS=(1 "RWKV-4-Claude-for-Mobile [Default]"
+         2 "RWKV-4 World"
+         3 "LLaMA-2-7B-Chat"
+         4 "Do not download AI Model (?) - [...]")
 
 
 CHOICE=$(dialog --clear \
@@ -23,24 +24,33 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
         1)
-        echo "You chose 'AI Model Auto-Detection (RWKV-4-Claude) [Default]...'";
+        echo "You chose 'RWKV-4-Claude-for-Mobile [Default]...'";
         {
             rm -rf 'altaera_install_pt-1.sh'
-            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-1-auto.sh -O 'altaera_install_pt-1.sh'
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-1-rwkv-4-claude-for-mobile.sh -O 'altaera_install_pt-1.sh'
             chmod a+x 'altaera_install_pt-1.sh'
          } &> /dev/null 2>&1;
             bash altaera_install_pt-1.sh
             ;;
         2)
-        echo "You chose 'AI Model Manual Selection...'";
+        echo "You chose 'RWKV-4 World...'";
         {
             rm -rf 'altaera_install_pt-1.sh'
-            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-1-manual.sh -O 'altaera_install_pt-1.sh'
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-1-rwkv-4-world.sh -O 'altaera_install_pt-1.sh'
             chmod a+x 'altaera_install_pt-1.sh'
          } &> /dev/null 2>&1;
             bash altaera_install_pt-1.sh
             ;;
         3)
+        echo "You chose 'LLaMA-2-7B-Chat...'";
+        {
+            rm -rf 'altaera_install_pt-1.sh'
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-1-llama-2-7b-chat.sh -O 'altaera_install_pt-1.sh'
+            chmod a+x 'altaera_install_pt-1.sh'
+         } &> /dev/null 2>&1;
+            bash altaera_install_pt-1.sh
+            ;;
+        4)
         echo "You chose 'Do not download AI Model (?) - [...]'";
         {
             rm -rf 'altaera_install_pt-1.sh'
