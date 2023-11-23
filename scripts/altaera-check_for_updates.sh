90069c6f663585a1ef97b348dac44a56a81bd4f8
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-23 13:22:47 +0100

    Update altaera-check_for_updates.sh

--- scripts/altaera-check_for_updates.sh
+++ scripts/altaera-check_for_updates.sh
@@ -18,7 +18,7 @@ case $response in
                 } &> /dev/null 2>&1;
  
          if [ $(bash 'AltaeraAI/altaera-version_upstream.sh'
-          ) = "v3.2.1" ]; then
+          ) = "v3.2.2" ]; then
           rm -rf 'AltaeraAI/altaera-version_upstream.sh'
           bash 'AltaeraAI/altaera-up_to_date.sh'
           exit
