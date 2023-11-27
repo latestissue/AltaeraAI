f2de92cc22f73cc6db063096434559901940759c
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-27 11:09:38 +0100

    Create altaera-list_models.sh

---
+++ scripts/altaera-artix/altaera-list_models.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+files=() #blank the variable so its empty for next use
+# Loop folder, add files to array
+while IFS= read -r -d $'\0' file; do
+    files+=("$file" "")
+done < <(find "/root/models" -maxdepth 1 -type f \( -iname \*.bin -o -iname \*.gguf \) -print0)
+# or for all files: done < <(find "/home" -maxdepth 1 -type f -print0)
+# Check it has at least 1 file to show (otherwise dialog errors)
+if [ ${#files[@]} -eq 0 ]; then
+    clear
+    echo "No .bin or .gguf files found in $directory"
+else
+    file=$(dialog --stdout --title "AltaeraAI - AI Models:" --menu "This is a list of your currently downloaded models:" 0 0 0 "${files[@]}")
+fi
+
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+
+clear
+exit
+
+else
+clear
+exit
+fi
