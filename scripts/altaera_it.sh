ea9d06cef66f950d2cba67ab9d90830065e942ff
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-11-30 13:39:04 +0100

    Update altaera_it.sh

--- scripts/altaera_it.sh
+++ scripts/altaera_it.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+cd '/data/data/com.termux/files/home'
+
 HEIGHT=30
 WIDTH=60
 CHOICE_HEIGHT=7
-BACKTITLE="AltaeraAI - v2.9.1"
+BACKTITLE="AltaeraAI - v3.2.5"
 TITLE="Benvenuti!"
 MENU="Cosa ti piacerebbe fare?"
 
 OPTIONS=(1 "Avviare AltaeraAI [KoboldCpp]"
          2 "Verificare la disponibilità degli aggiornamenti [...]"
          3 "Aggiornamento della forza [...]"
-         4 "Cambiare il modello di AI 🤖"
+         4 "Gestire i modelli AI 🤖 [...]"
          5 "Cambiare la lingua 🌐"
-         6 "ngrok Tunnelling sicuro [...]"
-         7 "Backup/Ripristino del modello AI (/sdcard) [...]"
+         6 "Cambiare le impostazioni di KoboldCpp [...]"
+         7 "ngrok Tunnelling sicuro [...]"
          8 "Reinstallare (riparare) AltaeraAI"
          9 "Disinstallare AltaeraAI"
          10 "Assistenza ai clienti [...]"
@@ -34,7 +36,7 @@ clear
 case $CHOICE in
         1)
             clear
-            proot-distro login altaera -- ./altaera.sh &
+            proot-distro login altaera -- ./altaera.sh
             ;;
         2)
             clear
@@ -48,7 +50,7 @@ case $CHOICE in
             ;;
         4)
             clear
-            './AltaeraAI/altaera-model.sh'
+            './AltaeraAI/altaera-manage_models.sh'
             './AltaeraAI/altaera.sh'
             ;;
         5)
@@ -58,12 +60,12 @@ case $CHOICE in
             ;;
         6)
             clear
-            './AltaeraAI/altaera-ngrok.sh'
+            './AltaeraAI/altaera-koboldcpp_settings.sh'
             './AltaeraAI/altaera.sh'
             ;;
         7)
             clear
-            './AltaeraAI/altaera-model_backup-restore.sh'
+            './AltaeraAI/altaera-ngrok.sh'
             './AltaeraAI/altaera.sh'
             ;;
         8)
@@ -88,5 +90,5 @@ case $CHOICE in
             ;;
         13)
             clear
-            ;;
+            exit
 esac
