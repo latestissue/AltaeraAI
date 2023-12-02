236769ef9e6772f310114057b75899f171168f24
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2023-12-02 13:19:36 +0100

    Update altaera-update.sh

--- scripts/altaera-update.sh
+++ scripts/altaera-update.sh
@@ -1,10 +1,10 @@
-echo "Updating AltaeraAI... Please do not exit Termux!";
-
+clear
+echo "Initializing update...";
 {
 rm -rf 'altaera-update_content.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content.sh
 chmod a+x 'altaera-update_content.sh'
-bash 'altaera-update_content.sh'
 } &> /dev/null 2>&1;
+bash 'altaera-update_content.sh'
 clear
 bash 'AltaeraAI/altaera-updated_successfully.sh'
