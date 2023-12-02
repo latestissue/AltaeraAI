aa2263850ef6bada406123637a46d814cf3a9993
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2023-12-02 13:25:09 +0100

    Menu UI visual enhancements

--- scripts/altaera-version_upstream.sh
+++ scripts/altaera-version_upstream.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "v3.2.6"
+echo "v3.2.7"
 
 exit
--- scripts/altaera_por.sh
+++ scripts/altaera_por.sh
@@ -5,7 +5,7 @@ cd '/data/data/com.termux/files/home'
 HEIGHT=30
 WIDTH=60
 CHOICE_HEIGHT=7
-BACKTITLE="AltaeraAI - v3.2.6"
+BACKTITLE="AltaeraAI - v3.2.7"
 TITLE="Bem-vindo!"
 MENU="O que é que gostaria de fazer?"
 
