1148219c89f359464fecf7ba598c90c3eff38bdb
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2023-12-14 13:28:14 +0100

    v3.3 >> v3.4

--- scripts/altaera-version_upstream.sh
+++ scripts/altaera-version_upstream.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "v3.3"
+echo "v3.4"
 
 exit
--- scripts/altaera_ukr.sh
+++ scripts/altaera_ukr.sh
@@ -5,7 +5,7 @@ cd '/data/data/com.termux/files/home'
 HEIGHT=30
 WIDTH=60
 CHOICE_HEIGHT=7
-BACKTITLE="AltaeraAI - v3.3"
+BACKTITLE="AltaeraAI - v3.4"
 TITLE="Ласкаво просимо!"
 MENU="Чим би ви хотіли займатися?"
 
