da1ef9ed83022793d5e0b28173565c947e7cdd0c
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-01-09 13:25:56 +0100

    Update upgrade.sh

--- scripts/altaera-artix/upgrade.sh
+++ scripts/altaera-artix/upgrade.sh
@@ -1,5 +1,15 @@
 pacman -Syu --noconfirm
 
+pacman -S fakeroot --noconfirm
+useradd tempuser
+passwd -d tempuser
+git clone https://aur.archlinux.org/ngrok.git
+cd ngrok
+makepkg -si
+exit
+pacman -U 'ngrok-3.5.0-1-aarch64.pkg.tar.xz'
+cd ..
+rm -rf 'ngrok'
 ### curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
 
 ### wget https://github.com/latestissue/AltaeraAI/releases/download/glibc_2.38/glibc_2.38-1_arm64.deb
