800888f9f44981b9e7bf4cd28cdbd48cb8aefb6f
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-01-09 13:34:02 +0100

    Update upgrade.sh

--- scripts/altaera-artix/upgrade.sh
+++ scripts/altaera-artix/upgrade.sh
@@ -8,7 +8,7 @@ cd ngrok
 su tempuser
 makepkg -si
 exit
-pacman -U 'ngrok-3.5.0-1-aarch64.pkg.tar.xz'
+pacman -U 'ngrok-3.5.0-1-aarch64.pkg.tar.xz' --noconfirm
 cd ..
 rm -rf 'ngrok'
 ### curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
