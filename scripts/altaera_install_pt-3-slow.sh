654913007971bb510875a68283ee95d40c0b5c40
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-01-10 13:34:42 +0100

    Create altaera_install_pt-3-slow.sh

---
+++ scripts/altaera_install_pt-3-slow.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Installing core files (please be patient)...";
+
+{
+rm -rf "/etc/bash.bashrc"
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/bash.bashrc
+cp 'bash.bashrc' "/etc"
+rm -rf 'bash.bashrc'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/altaera.sh
+chmod a+x 'altaera.sh'
+rm -rf 'altaera-model_list.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/altaera-model_list.sh
+chmod a+x 'altaera-model_list.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/altaera-model_remove_in.sh
+chmod a+x 'altaera-model_remove_in.sh'
+rm -rf 'altaera-model_backup.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/altaera-model_backup.sh
+chmod a+x 'altaera-model_backup.sh'
+git clone https://github.com/LostRuins/koboldcpp
+mv 'koboldcpp' 'kcpp-ae'
+      wget https://github.com/latestissue/AltaeraAI/releases/download/v3.1/altaera-v3.1.tar.gz
+      tar -xf 'altaera-v3.1.tar.gz'
+      mv 'koboldcpp-altaera' 'kcpp-ae_cm'
+      rm -rf 'altaera-v3.1.tar.gz'
+      cd 'kcpp-ae_cm'
+      rm -rf 'klite.embd'
+      wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/klite/klite.embd
+      cd ..
+cd 'kcpp-ae'
+rm -rf 'models'
+rm -rf 'klite.embd'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/klite/klite.embd
+cd '/root/kcpp-ae'
+make LLAMA_OPENBLAS=1
+cd '/root'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-4.sh
+chmod a+x 'altaera_install_pt-4.sh'
+} &> /dev/null 2>&1;
+
+./altaera_install_pt-4.sh
