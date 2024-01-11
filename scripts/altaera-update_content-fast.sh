79194960bd82efa6068e5dca8318056ddaa685f5
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-01-11 13:07:04 +0100

    Update altaera-update_content-fast.sh

--- scripts/altaera-update_content-fast.sh
+++ scripts/altaera-update_content-fast.sh
@@ -6,6 +6,9 @@ echo "Updating AltaeraAI shell files...
 mkdir '/data/data/com.termux/files/home/AltaeraAI'
 mv 'altaera.sh' 'altaeralogin.sh' 'altaera-lang.sh' 'altaera-model' 'altaera-model-rwkv-4.sh' 'altaera-update.sh' 'altaera-update-proot.sh' 'altaera-ngrok.sh' 'altaera-ngrok-configuration.sh' 'altaera-ngrok-on.sh' 'altaera-reinstall.sh' 'altaera-uninstall.sh' './AltaeraAI'
 cd 'AltaeraAI'
+rm -rf 'altaera-update.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update.sh
+chmod a+x 'altaera-update.sh'
 rm -rf 'altaera.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera.sh
 chmod a+x 'altaera.sh'
