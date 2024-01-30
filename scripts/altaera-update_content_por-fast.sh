ffe476cc73405264ea3463bd2fe50fe87f4e1c98
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-01-30 14:09:48 +0100

    Update and rename altaera-update_content_por.sh to altaera-update_content_por-fast.sh

--- scripts/altaera-update_content_por-fast.sh
+++ scripts/altaera-update_content_por-fast.sh
@@ -1,9 +1,14 @@
-echo "Updating AltaeraAI...";
+echo "Atualização dos ficheiros da shell AltaeraAI...
+
+";
 
 {
 mkdir '/data/data/com.termux/files/home/AltaeraAI'
 mv 'altaera.sh' 'altaeralogin.sh' 'altaera-lang.sh' 'altaera-model' 'altaera-model-rwkv-4.sh' 'altaera-update.sh' 'altaera-update-proot.sh' 'altaera-ngrok.sh' 'altaera-ngrok-configuration.sh' 'altaera-ngrok-on.sh' 'altaera-reinstall.sh' 'altaera-uninstall.sh' './AltaeraAI'
 cd 'AltaeraAI'
+rm -rf 'altaera-update.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update.sh
+chmod a+x 'altaera-update.sh'
 rm -rf 'altaera.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_por.sh -O 'altaera.sh'
 chmod a+x 'altaera.sh'
@@ -44,8 +49,6 @@ chmod a+x 'altaera-check_for_updates.sh'
 rm -rf 'altaera-up_to_date.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-up_to_date.sh
 chmod a+x 'altaera-up_to_date.sh'
-wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-up_to_date_por.sh -O 'altaera-up_to_date.sh'
-chmod a+x 'altaera-up_to_date.sh'
 rm -rf 'altaera-update_available.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_available_por.sh -O 'altaera-update_available.sh'
 chmod a+x 'altaera-update_available.sh'
@@ -77,6 +80,24 @@ chmod a+x 'altaera-reinstall.sh'
 rm -rf 'altaera-uninstall.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-uninstall.sh
 chmod a+x 'altaera-uninstall.sh'
+rm -rf 'altaera-customer_support.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-customer_support.sh
+chmod a+x 'altaera-customer_support.sh'
+rm -rf 'altaera-koboldcpp_settings.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-koboldcpp_settings.sh
+chmod a+x 'altaera-koboldcpp_settings.sh'
+rm -rf 'altaera-koboldcpp_settings_contextsize.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-koboldcpp_settings_contextsize.sh
+chmod a+x 'altaera-koboldcpp_settings_contextsize.sh'
+rm -rf 'altaera-koboldcpp_settings_blasbatchsize.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-koboldcpp_settings_blasbatchsize.sh
+chmod a+x 'altaera-koboldcpp_settings_blasbatchsize.sh'
+rm -rf 'altaera-manage_models.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-manage_models.sh
+chmod a+x 'altaera-manage_models.sh'
+rm -rf 'altaera-model_remove.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model_remove.sh
+chmod a+x 'altaera-model_remove.sh'
 
 rm -rf 'altaera-model_backup-restore.sh'
 rm -rf 'altaera-model_backup.sh'
@@ -91,6 +112,13 @@ wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaer
 chmod a+x 'altaera-model_backup-restore.sh'
 chmod a+x 'altaera-model_restore.sh'
 
+} &> /dev/null 2>&1;
+
+echo "Atualização dos ficheiros principais do AltaeraAI...
+
+";
+
+{
 mv '/data/data/com.termux/files/home/AltaeraAI-temp' '/data/data/com.termux/files/home/AltaeraAI-tmp'
 
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/bash.bashrc
@@ -107,7 +135,7 @@ rm -rf 'upgrade.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/upgrade.sh
 chmod a+x 'upgrade.sh'
 rm -rf 'ngrok-authtoken.sh'
-wget https://github.com/latestissue/AltaeraAI/releases/download/v3.7/altaera-v3.9.tar.gz
+wget https://github.com/latestissue/AltaeraAI/releases/download/v3.9/altaera-v3.9.tar.gz
 tar -xf 'altaera-v3.9.tar.gz'
 cd 'kcpp-ae'
 rm -rf 'models'
@@ -121,12 +149,16 @@ mv '/data/data/com.termux/files/home/AltaeraAI-tmp/models' '/data/data/com.termu
 cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
 mkdir '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/kcpp-ae/prompts'
 cp '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/temporary/chat-with-bob.txt' '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/kcpp-ae/prompts'
+rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.3.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.4.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.4.1.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.5.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.6.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.7.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.8.tar.gz'
+rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.9.tar.gz'
+rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v3.0.tar.gz'
+rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v3.1.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v3.9.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-temporary'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/chat-with-bob.txt'
@@ -137,23 +169,26 @@ rm -rf '/data/data/com.termux/files/home/altaera_install_pt-1.sh'
 rm -rf '/data/data/com.termux/files/home/altaera_install_pt-2.sh'
 rm -rf '/data/data/com.termux/files/home/altaera-update_content.sh'
 rm -rf '/data/data/com.termux/files/home/altaera_install_model.sh'
-mv '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/kcpp-ae/RWKV-model.bin' '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/kcpp-ae/model.bin'
 cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
 rm -rf 'klite.embd'
 rm -rf 'altaera.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/altaera.sh
 chmod a+x 'altaera.sh'
+rm -rf 'altaera-model_list.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/altaera-model_list.sh
+chmod a+x 'altaera-model_list.sh'
+rm -rf 'altaera-model_remove_in.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/altaera-model_remove_in.sh
+chmod a+x 'altaera-model_remove_in.sh'
+rm -rf 'altaera-model_backup.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/altaera-model_backup.sh
+chmod a+x 'altaera-model_backup.sh'
 cd '/data/data/com.termux/files/home'
 } &> /dev/null 2>&1;
 
-echo "Atualização do ambiente AltaeraAI PRoot-Distro (Artix Linux)...";
+echo "Atualização do ambiente da distro AltaeraAI PRoot (Artix Linux)...";
 
 {
 proot-distro login altaera -- ./upgrade.sh &
 clear
 } &> /dev/null 2>&1;
-echo "
-
-
-      Update completed!
-      "
