51d54634abc2f12bf863b9ef264921aed5bf3b37
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-01-30 14:34:18 +0100

    Update and rename altaera-update_content_spa.sh to altaera-update_content_spa-fast.sh

--- scripts/altaera-update_content_spa-fast.sh
+++ scripts/altaera-update_content_spa-fast.sh
@@ -1,9 +1,14 @@
-echo "Actualizando AltaeraAI...";
+echo "Updating AltaeraAI shell files...
+
+";
 
 {
 mkdir '/data/data/com.termux/files/home/AltaeraAI'
 mv 'altaera.sh' 'altaeralogin.sh' 'altaera-lang.sh' 'altaera-model' 'altaera-model-rwkv-4.sh' 'altaera-update.sh' 'altaera-update-proot.sh' 'altaera-ngrok.sh' 'altaera-ngrok-configuration.sh' 'altaera-ngrok-on.sh' 'altaera-reinstall.sh' 'altaera-uninstall.sh' './AltaeraAI'
 cd 'AltaeraAI'
+rm -rf 'altaera-update.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update.sh
+chmod a+x 'altaera-update.sh'
 rm -rf 'altaera.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_spa.sh -O 'altaera.sh'
 chmod a+x 'altaera.sh'
@@ -75,6 +80,24 @@ chmod a+x 'altaera-reinstall.sh'
 rm -rf 'altaera-uninstall.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-uninstall.sh
 chmod a+x 'altaera-uninstall.sh'
+rm -rf 'altaera-customer_support.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-customer_support.sh
+chmod a+x 'altaera-customer_support.sh'
+rm -rf 'altaera-koboldcpp_settings.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-koboldcpp_settings.sh
+chmod a+x 'altaera-koboldcpp_settings.sh'
+rm -rf 'altaera-koboldcpp_settings_contextsize.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-koboldcpp_settings_contextsize.sh
+chmod a+x 'altaera-koboldcpp_settings_contextsize.sh'
+rm -rf 'altaera-koboldcpp_settings_blasbatchsize.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-koboldcpp_settings_blasbatchsize.sh
+chmod a+x 'altaera-koboldcpp_settings_blasbatchsize.sh'
+rm -rf 'altaera-manage_models.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-manage_models.sh
+chmod a+x 'altaera-manage_models.sh'
+rm -rf 'altaera-model_remove.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model_remove.sh
+chmod a+x 'altaera-model_remove.sh'
 
 rm -rf 'altaera-model_backup-restore.sh'
 rm -rf 'altaera-model_backup.sh'
@@ -89,6 +112,13 @@ wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaer
 chmod a+x 'altaera-model_backup-restore.sh'
 chmod a+x 'altaera-model_restore.sh'
 
+} &> /dev/null 2>&1;
+
+echo "Actualizando los archivos del núcleo de AltaeraAI...
+
+";
+
+{
 mv '/data/data/com.termux/files/home/AltaeraAI-temp' '/data/data/com.termux/files/home/AltaeraAI-tmp'
 
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/bash.bashrc
@@ -105,7 +135,7 @@ rm -rf 'upgrade.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/upgrade.sh
 chmod a+x 'upgrade.sh'
 rm -rf 'ngrok-authtoken.sh'
-wget https://github.com/latestissue/AltaeraAI/releases/download/v3.7/altaera-v3.9.tar.gz
+wget https://github.com/latestissue/AltaeraAI/releases/download/v3.9/altaera-v3.9.tar.gz
 tar -xf 'altaera-v3.9.tar.gz'
 cd 'kcpp-ae'
 rm -rf 'models'
@@ -126,6 +156,9 @@ rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/al
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.6.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.7.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.8.tar.gz'
+rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v2.9.tar.gz'
+rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v3.0.tar.gz'
+rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v3.1.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-v3.9.tar.gz'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-temporary'
 rm -rf '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/chat-with-bob.txt'
@@ -136,23 +169,26 @@ rm -rf '/data/data/com.termux/files/home/altaera_install_pt-1.sh'
 rm -rf '/data/data/com.termux/files/home/altaera_install_pt-2.sh'
 rm -rf '/data/data/com.termux/files/home/altaera-update_content.sh'
 rm -rf '/data/data/com.termux/files/home/altaera_install_model.sh'
-mv '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/kcpp-ae/RWKV-model.bin' '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/kcpp-ae/model.bin'
 cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
 rm -rf 'klite.embd'
 rm -rf 'altaera.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/altaera.sh
 chmod a+x 'altaera.sh'
+rm -rf 'altaera-model_list.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/altaera-model_list.sh
+chmod a+x 'altaera-model_list.sh'
+rm -rf 'altaera-model_remove_in.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/altaera-model_remove_in.sh
+chmod a+x 'altaera-model_remove_in.sh'
+rm -rf 'altaera-model_backup.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-artix/altaera-model_backup.sh
+chmod a+x 'altaera-model_backup.sh'
 cd '/data/data/com.termux/files/home'
 } &> /dev/null 2>&1;
 
-echo "Actualización del entorno AltaeraAI PRoot-Distro (Artix Linux)...";
+echo "Actualización del entorno AltaeraAI PRoot Distro (Artix Linux)...";
 
 {
 proot-distro login altaera -- ./upgrade.sh &
 clear
 } &> /dev/null 2>&1;
-echo "
-
-
-      Update completed!
-      "
