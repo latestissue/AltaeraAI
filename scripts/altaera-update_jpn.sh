934d16d0d01da77944b1f1cb58d2e8891e620449
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-01-30 12:11:21 +0100

    Update altaera-update_jpn.sh

--- scripts/altaera-update_jpn.sh
+++ scripts/altaera-update_jpn.sh
@@ -1,10 +1,54 @@
-echo "Updating AltaeraAI... Please do not exit Termux!";
+#!/bin/bash
 
-{
-rm -rf 'altaera-update_content.sh'
-wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content_jpn.sh -O 'altaera-update_content.sh'
-chmod a+x 'altaera-update_content.sh'
-bash 'altaera-update_content.sh'
-} &> /dev/null 2>&1;
+HEIGHT=10
+WIDTH=100
+CHOICE_HEIGHT=24
+BACKTITLE="AltaeraAI - 更新"
+TITLE="更新方法の選択"
+MENU="パッケージ化されたKoboldCppでアップデートしますか？遅い"
+
+OPTIONS=(1 "パッケージ化されたKoboldCpp [高速化]によるアップデート"
+         2 "KoboldCppを自作するアップデート [遅い]。")
+
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
 clear
-bash 'AltaeraAI/altaera-updated_successfully.sh'
+case $CHOICE in
+
+        1)
+            clear
+            echo "アップデートの初期化中...
+            
+            ";
+            {
+            rm -rf 'altaera-update_content.sh'
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content_jpn-fast.sh -O 'altaera-update_content.sh'
+            chmod a+x 'altaera-update_content.sh'
+            } &> /dev/null 2>&1;
+            bash 'altaera-update_content.sh'
+            clear
+            bash 'AltaeraAI/altaera-updated_successfully.sh'
+            ;;
+        2)
+            clear
+            echo "アップデートの初期化中...
+            
+            ";
+            {
+            rm -rf 'altaera-update_content'
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content_jpn-slow.sh -O 'altaera-update_content.sh'
+            chmod a+x 'altaera-update_content.sh'
+            } &> /dev/null 2>&1;
+            bash 'altaera-update_content.sh'
+            clear
+            bash 'AltaeraAI/altaera-updated_successfully.sh'
+            ;;
+            
+esac
