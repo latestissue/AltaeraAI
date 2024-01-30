9f2d68bbd5d1b3c629544614101e1f7604d5c813
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-01-30 13:28:54 +0100

    Update altaera-update_pol.sh

--- scripts/altaera-update_pol.sh
+++ scripts/altaera-update_pol.sh
@@ -1,10 +1,54 @@
-echo "Updating AltaeraAI... Please do not exit Termux!";
+#!/bin/bash
 
-{
-rm -rf 'altaera-update_content.sh'
-wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content_pol.sh -O 'altaera-update_content.sh'
-chmod a+x 'altaera-update_content.sh'
-bash 'altaera-update_content.sh'
-} &> /dev/null 2>&1;
+HEIGHT=10
+WIDTH=100
+CHOICE_HEIGHT=24
+BACKTITLE="AltaeraAI - Aktualizacja"
+TITLE="Wybór metody aktualizacji"
+MENU="Czy chcesz zaktualizować za pomocą wstępnie spakowanego KoboldCpp [Szybciej], czy budując własny? [Wolniej]"
+
+OPTIONS=(1 "Aktualizacja z wstępnie spakowanym KoboldCpp [Szybciej]"
+         2 "Aktualizacja z budowaniem własnego KoboldCpp [Wolniej]")
+
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
 clear
-bash 'AltaeraAI/altaera-updated_successfully.sh'
+case $CHOICE in
+
+        1)
+            clear
+            echo "Inicjowanie aktualizacji...
+            
+            ";
+            {
+            rm -rf 'altaera-update_content.sh'
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content_pol-fast.sh -O 'altaera-update_content.sh'
+            chmod a+x 'altaera-update_content.sh'
+            } &> /dev/null 2>&1;
+            bash 'altaera-update_content.sh'
+            clear
+            bash 'AltaeraAI/altaera-updated_successfully.sh'
+            ;;
+        2)
+            clear
+            echo "Inicjowanie aktualizacji...
+            
+            ";
+            {
+            rm -rf 'altaera-update_content'
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content_pol-slow.sh -O 'altaera-update_content.sh'
+            chmod a+x 'altaera-update_content.sh'
+            } &> /dev/null 2>&1;
+            bash 'altaera-update_content.sh'
+            clear
+            bash 'AltaeraAI/altaera-updated_successfully.sh'
+            ;;
+            
+esac
