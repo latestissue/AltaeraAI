3af50ff3505b204f3f91aebe92fe6f34c44f912a
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-01-31 17:01:12 +0100

    Update altaera-fast.sh

--- scripts/altaera-artix/proot/altaera-fast.sh
+++ scripts/altaera-artix/proot/altaera-fast.sh
@@ -1,13 +1,17 @@
-DISTRO_NAME="Artix_AltaeraAI"
-DISTRO_COMMENT="Artix Linux utilised by the AltaeraAI project"
+DISTRO_NAME="Arch Linux - AltaeraAI"
+DISTRO_COMMENT="Arch Linux utilised by the AltaeraAI project"
 
-TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v3.18.1/artix-aarch64-pd-v3.18.1.tar.xz"
-TARBALL_SHA256['aarch64']="9801031864da6bc9dc69293695797f8aba7866c273bb7403f95c4e70be987936"
+
+TARBALL_URL['arm']="https://github.com/termux/proot-distro/releases/download/v4.6.0/archlinux-arm-pd-v4.6.0.tar.xz"
+TARBALL_SHA256['arm']="9edc60150ffdeae42b05fdcffdf06226641c442673f66b64af369504abe83a4b"
+
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.6.0/archlinux-aarch64-pd-v4.6.0.tar.xz"
+TARBALL_SHA256['aarch64']="7e87d551845aedae5a111d1fdcc2f5a69b0805f365244f3fab3fe67cd4114f00"
 
 distro_setup() {
 	run_proot_cmd pacman -Syu --noconfirm
-	run_proot_cmd pacman -S curl wget procps git clang make python3 blas-openblas fakeroot --noconfirm
-	run_proot_cmd wget --no-check-certificate https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-3-fast.sh -O 'altaera_install_pt-3.sh'
+	run_proot_cmd pacman -S curl wget procps --noconfirm
+	run_proot_cmd wget --no-check-certificate https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-3-fast.sh
  	run_proot_cmd chmod a+x 'altaera_install_pt-3.sh'
  	run_proot_cmd bash 'altaera_install_pt-3.sh'
 }
