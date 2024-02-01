07433c590a41fb3d3eecaf16a686f061386a587b
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-02-01 10:12:47 +0100

    Update altaera_install_pt-4.sh

--- scripts/altaera_install_pt-4.sh
+++ scripts/altaera_install_pt-4.sh
@@ -9,6 +9,20 @@ pacman -U 'ngrok-3.5.0-1-aarch64.pkg.tar.xz' --noconfirm
 rm -rf 'ngrok-3.5.0-1-aarch64.pkg.tar.xz'
 } &> /dev/null 2>&1;
 
+{
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-print_architecture.sh
+chmod a+x 'altaera-print_architecture.sh'
+
+         if [ $(bash 'altaera-print_architecture.sh'
+          ) = "arm" ]; then
+          pacman -Rcns 'blas-openblas' --noconfirm
+          rm -rf 'altaera-print_architecture.sh'
+          else
+          rm -rf 'altaera-print_architecture.sh'
+        fi
+        
+} &> /dev/null 2>&1;
+
 echo "Cleaning up File System...";
 
 {
