68ba4b306156f0270d36b04ab2c71450fca10730
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-02-01 11:30:51 +0100

    Create install_architecture.sh

---
+++ scripts/install_architecture.sh
@@ -0,0 +1,22 @@
+{
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-print_architecture.sh
+chmod a+x 'altaera-print_architecture.sh'
+
+         if [ $(bash 'altaera-print_architecture.sh'
+          ) = "arm" ]; then
+          
+            cd $PREFIX/etc/proot-distro
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-proot/rootfs/altaera-slow.sh -O 'altaera.sh' -q --show-progress
+        
+          rm -rf '/data/data/com.termux/files/home/altaera-print_architecture.sh'
+          
+          else
+          
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-install_method.sh -q --show-progress
+            chmod a+x 'altaera-install_method.sh'
+            
+          rm -rf '/data/data/com.termux/files/home/altaera-print_architecture.sh'
+          
+        fi
+        
+} &> /dev/null 2>&1;
