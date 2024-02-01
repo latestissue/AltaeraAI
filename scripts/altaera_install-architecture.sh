a4b5712e9b09afcb283db6973bd7b29cedb484ac
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-02-01 11:35:13 +0100

    Create altaera_install-architecture.sh

---
+++ scripts/altaera_install-architecture.sh
@@ -0,0 +1,13 @@
+{
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-print_architecture.sh
+chmod a+x 'altaera-print_architecture.sh'
+
+         if [ $(bash 'altaera-print_architecture.sh'
+          ) = "arm" ]; then
+          pacman -Rcns 'blas-openblas' --noconfirm
+          rm -rf 'altaera-print_architecture.sh'
+          else
+          rm -rf 'altaera-print_architecture.sh'
+        fi
+        
+} &> /dev/null 2>&1;
