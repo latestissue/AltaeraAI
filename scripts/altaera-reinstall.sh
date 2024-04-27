4765ce998624a65971471d4beb1b1ce06ce16d0c
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-04-27 14:24:19 +0200

    Update altaera-reinstall.sh

--- scripts/altaera-reinstall.sh
+++ scripts/altaera-reinstall.sh
@@ -28,8 +28,6 @@ case $CHOICE in
         2)
             echo "You chose 'Reinstall without re-downloading the AI model (faster)'...";
             {
-	    rm -rf 'AltaeraAI'
-	    mkdir 'AltaeraAI'
             cd 'AltaeraAI-tmp'
             wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_reinstall_keep_model.sh
             chmod a+x 'altaera_reinstall_keep_model.sh'
@@ -42,8 +40,6 @@ case $CHOICE in
         3)
             echo "You chose 'Reinstall everything, including the AI model (slower)'...";
             {
-	    rm -rf 'AltaeraAI'
-	    mkdir 'AltaeraAI'
 	    mkdir 'AltaeraAI-tmp/models'
             cd 'AltaeraAI-tmp'
             wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_reinstall_everything.sh
