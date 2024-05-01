07266af47e35192b32d3045c43b52f310d1d7149
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-01 17:54:04 +0200

    Update altaera_pre-update.sh

--- scripts/altaera_pre-update.sh
+++ scripts/altaera_pre-update.sh
@@ -7,11 +7,12 @@
                 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-version_package.sh
                 chmod a+x 'altaera-version_package.sh'
                 cd ..
-                } &> /dev/null 2>&1;
  
          if [ $(bash 'AltaeraAI/altaera-version_package.sh'
           ) = "v4.7.tar.gz" ]; then
+          cd "AltaeraAI"
           rm -rf 'AltaeraAI/altaera-version_package.sh'
+          rm -rf 'altaera-update_content.sh'
           wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content-scripts.sh -O 'altaera-update_content.sh'
           chmod a+x 'altaera-update_content.sh'
           bash 'altaera-update_content.sh'
@@ -20,4 +21,5 @@
           exit
           else
           bash 'AltaeraAI/altaera-update.sh'
-        fi
+         fi
+         } &> /dev/null 2>&1;
