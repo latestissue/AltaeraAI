fe0a63795b812dda51c4e9137be8e2e164cff032
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-01 17:26:10 +0200

    Create altaera-update_content-scripts.sh

---
+++ scripts/altaera-update_content-scripts.sh
@@ -0,0 +1,134 @@
+echo "Updating AltaeraAI shell files...
+
+";
+
+{
+mkdir '/data/data/com.termux/files/home/AltaeraAI'
+mv 'altaera.sh' 'altaeralogin.sh' 'altaera-lang.sh' 'altaera-model' 'altaera-model-rwkv-4.sh' 'altaera-update.sh' 'altaera-update-proot.sh' 'altaera-ngrok.sh' 'altaera-ngrok-configuration.sh' 'altaera-ngrok-on.sh' 'altaera-reinstall.sh' 'altaera-uninstall.sh' './AltaeraAI'
+cd 'AltaeraAI'
+rm -rf 'altaera-update.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update.sh
+chmod a+x 'altaera-update.sh'
+rm -rf 'altaera.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera.sh
+chmod a+x 'altaera.sh'
+rm -rf 'altaeralogin.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaeralogin.sh
+chmod a+x 'altaeralogin.sh'
+rm -rf 'lang.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-proot/lang.sh
+chmod a+x 'lang.sh'
+rm -rf 'altaera-lang.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-lang.sh
+chmod a+x 'altaera-lang.sh'
+rm -rf 'altaera-model.sh'
+rm -rf '/data/data/com.termux/files/home/altaera-model.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model.sh
+chmod a+x 'altaera-model.sh'
+rm -rf 'altaera-model-rwkv-4-world-claude-for-mobile.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model-rwkv-4-world-claude-for-mobile.sh
+chmod a+x 'altaera-model-rwkv-4-world-claude-for-mobile.sh'
+rm -rf 'altaera-model-rwkv-4.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model-rwkv-4.sh
+chmod a+x 'altaera-model-rwkv-4.sh'
+rm -rf 'altaera-model-llama-2-7b-chat.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model-llama-2-7b-chat.sh
+chmod a+x 'altaera-model-llama-2-7b-chat.sh'
+rm -rf 'altaera-model-tinyllama-1.1b-chat-v1.0.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model-tinyllama-1.1b-chat-v1.0.sh
+chmod a+x 'altaera-model-tinyllama-1.1b-chat-v1.0.sh'
+rm -rf 'altaera-model-kobbletiny.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model-kobbletiny.sh
+chmod a+x 'altaera-model-kobbletiny.sh'
+rm -rf 'altaera-model-mamba.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model-mamba.sh
+chmod a+x 'altaera-model-mamba.sh'
+rm -rf 'altaera-model-llama-3-8b-instruct.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model-llama-3-8b-instruct.sh
+chmod a+x 'altaera-model-llama-3-8b-instruct.sh'
+rm -rf 'altaera-model-mistral-7b-instruct-v0.2.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model-mistral-7b-instruct-v0.2.sh
+chmod a+x 'altaera-model-mistral-7b-instruct-v0.2.sh'
+rm -rf 'altaera-model-phi-2-dpo.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model-phi-2-dpo.sh
+chmod a+x 'altaera-model-phi-2-dpo.sh'
+rm -rf 'altaera-model-vicuna-7b-v1.5.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model-vicuna-7b-v1.5.sh
+chmod a+x 'altaera-model-vicuna-7b-v1.5.sh'
+rm -rf 'altaera-check_for_updates.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-check_for_updates.sh
+chmod a+x 'altaera-check_for_updates.sh'
+rm -rf 'altaera-up_to_date.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-up_to_date.sh
+chmod a+x 'altaera-up_to_date.sh'
+rm -rf 'altaera-update_available.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_available.sh
+chmod a+x 'altaera-update_available.sh'
+rm -rf 'altaera-update.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update.sh
+chmod a+x 'altaera-update.sh'
+rm -rf 'altaera-updated_successfully.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-updated_successfully.sh
+chmod a+x 'altaera-updated_successfully.sh'
+rm -rf 'altaera-force_update.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-force_update.sh
+chmod a+x 'altaera-force_update.sh'
+rm -rf 'altaera-update-proot.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update-proot.sh
+chmod a+x 'altaera-update-proot.sh'
+rm -rf 'altaera-ngrok.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-ngrok.sh
+chmod a+x 'altaera-ngrok.sh'
+rm -rf 'altaera-ngrok-configuration.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-ngrok-configuration.sh
+chmod a+x 'altaera-ngrok-configuration.sh'
+rm -rf 'bash.bashrc'
+rm -rf 'altaera-ngrok-on.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-ngrok-on.sh
+chmod a+x 'altaera-ngrok-on.sh'
+rm -rf 'altaera-reinstall.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-reinstall.sh
+chmod a+x 'altaera-reinstall.sh'
+rm -rf 'altaera-uninstall.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-uninstall.sh
+chmod a+x 'altaera-uninstall.sh'
+rm -rf 'altaera-customer_support.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-customer_support.sh
+chmod a+x 'altaera-customer_support.sh'
+rm -rf 'altaera-koboldcpp_settings.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-koboldcpp_settings.sh
+chmod a+x 'altaera-koboldcpp_settings.sh'
+rm -rf 'altaera-koboldcpp_settings_contextsize.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-koboldcpp_settings_contextsize.sh
+chmod a+x 'altaera-koboldcpp_settings_contextsize.sh'
+rm -rf 'altaera-koboldcpp_settings_blasbatchsize.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-koboldcpp_settings_blasbatchsize.sh
+chmod a+x 'altaera-koboldcpp_settings_blasbatchsize.sh'
+rm -rf 'altaera-manage_models.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-manage_models.sh
+chmod a+x 'altaera-manage_models.sh'
+rm -rf 'altaera-model_remove.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model_remove.sh
+chmod a+x 'altaera-model_remove.sh'
+
+rm -rf 'altaera-model_backup-restore.sh'
+rm -rf 'altaera-model_backup.sh'
+rm -rf 'altaera-model_restore.sh'
+rm -rf 'altaera-model_backed-up.sh'
+rm -rf 'altaera-model_restored-cleanup.sh'
+rm -rf 'altaera-model_backup-cleaned-up.sh'
+
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model_backup-restore.sh
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-model_restore.sh
+
+chmod a+x 'altaera-model_backup-restore.sh'
+chmod a+x 'altaera-model_restore.sh'
+
+} &> /dev/null 2>&1;
+
+echo "Updating AltaeraAI PRoot Distro environment (Artix Linux)...";
+
+{
+proot-distro login altaera -- ./upgrade.sh &
+clear
+} &> /dev/null 2>&1;
