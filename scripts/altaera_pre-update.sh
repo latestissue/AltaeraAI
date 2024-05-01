49bfd275f44c7dabca1d143ea8fc2ead83eabb84
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-01 18:20:51 +0200

    Update altaera_pre-update.sh

--- scripts/altaera_pre-update.sh
+++ scripts/altaera_pre-update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-        echo "Establishing whether a package update is needed...";
+        echo "Establishing whether a package update is needed...
+        ";
         
                 {
                 cd "AltaeraAI"
@@ -16,7 +17,6 @@
           rm -rf 'altaera-update_content.sh'
           {
           wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content-scripts.sh -O 'altaera-update_content.sh'
-          clear
           chmod a+x 'altaera-update_content.sh'
           } &> /dev/null 2>&1;
           bash 'altaera-update_content.sh'
