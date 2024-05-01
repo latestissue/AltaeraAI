a495002288effb87d84e4d825f80062c8ffd9afc
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-01 17:57:25 +0200

    Update altaera_pre-update.sh

--- scripts/altaera_pre-update.sh
+++ scripts/altaera_pre-update.sh
@@ -15,6 +15,7 @@
           rm -rf 'altaera-update_content.sh'
           wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content-scripts.sh -O 'altaera-update_content.sh'
           chmod a+x 'altaera-update_content.sh'
+          } &> /dev/null 2>&1;
           bash 'altaera-update_content.sh'
           clear
           bash 'AltaeraAI/altaera-updated_successfully.sh'
@@ -22,4 +23,3 @@
           else
           bash 'AltaeraAI/altaera-update.sh'
          fi
-         } &> /dev/null 2>&1;
