a9b97eb26d178776182ab52fdcf3340f1b918893
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-01 16:16:55 +0200

    Update altaera-model-kobbletiny.sh

--- scripts/altaera-model-kobbletiny.sh
+++ scripts/altaera-model-kobbletiny.sh
@@ -5,10 +5,12 @@ WIDTH=160
 CHOICE_HEIGHT=24
 BACKTITLE="AltaeraAI - AI Model Settings"
 TITLE="AI Model Selection"
-MENU="Choose your desired KobbleTiny-GGUF model size/strategy:"
+MENU="Choose your desired KobbleTinyV2-1.1B-GGUF model size/strategy:"
 
 OPTIONS=(1 "KobbleTiny-Q4_K.gguf"
-         2 "KobbleTiny-f16.gguf")
+         2 "KobbleTiny-Q6_K.gguf"
+         3 "KobbleTiny-Q8_0.gguf"
+         4 "KobbleTiny-f16.gguf")
 
 
 CHOICE=$(dialog --clear \
@@ -30,11 +32,35 @@ case $CHOICE in
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/models'
             rm -rf 'RWKV-model.bin'
             rm -rf 'model.bin'
-            wget https://huggingface.co/concedo/KobbleTiny-GGUF/resolve/main/KobbleTiny-Q4_K.gguf -O 'Q4_K-KobbleTiny.gguf' -q --show-progress
+            wget https://huggingface.co/concedo/KobbleTinyV2-1.1B-GGUF/resolve/main/KobbleTiny-Q4_K.gguf -O 'Q4_K-KobbleTiny.gguf' -q --show-progress
             cd /root
             clear
             ;;
         2)
+            echo "You chose 'KobbleTiny-Q6_K.gguf'"
+            {
+            mkdir '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/models'
+            } &> /dev/null 2>&1;
+            cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/models'
+            rm -rf 'RWKV-model.bin'
+            rm -rf 'model.bin'
+            wget https://huggingface.co/concedo/KobbleTinyV2-1.1B-GGUF/resolve/main/KobbleTiny-Q6_K.gguf -O 'Q6_K-KobbleTiny.gguf' -q --show-progress
+            cd /root
+            clear
+            ;;
+        3)
+            echo "You chose 'KobbleTiny-Q8_0.gguf'"
+            {
+            mkdir '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/models'
+            } &> /dev/null 2>&1;
+            cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/models'
+            rm -rf 'RWKV-model.bin'
+            rm -rf 'model.bin'
+            wget https://huggingface.co/concedo/KobbleTinyV2-1.1B-GGUF/resolve/main/KobbleTiny-Q8_0.gguf -O 'Q8_0-KobbleTiny.gguf' -q --show-progress
+            cd /root
+            clear
+            ;;
+        4)
             echo "You chose 'KobbleTiny-f16.gguf'"
             {
             mkdir '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/models'
@@ -42,7 +68,7 @@ case $CHOICE in
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/models'
             rm -rf 'RWKV-model.bin'
             rm -rf 'model.bin'
-            wget https://huggingface.co/concedo/KobbleTiny-GGUF/resolve/main/KobbleTiny-f16.gguf -O 'f16-KobbleTiny.gguf' -q --show-progress
+            wget https://huggingface.co/concedo/KobbleTinyV2-1.1B-GGUF/resolve/main/KobbleTiny-f16.gguf -O 'f16-KobbleTiny.gguf' -q --show-progress
             cd /root
             clear
             ;;
