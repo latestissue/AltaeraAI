139d3d246d3de9aced4ab16f9ccced70a6a65942
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-02 15:57:11 +0200

    Create altaera-koboldcpp_settings_flashattention.sh

---
+++ scripts/altaera-koboldcpp_settings_flashattention.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+HEIGHT=20
+WIDTH=40
+CHOICE_HEIGHT=24
+BACKTITLE="AltaeraAI - KoboldCpp Settings"
+TITLE="Flash Attention (Experimental)"
+MENU="Choose whether to enable or disable Flash Attention:"
+
+OPTIONS=(1 "[...] Go Back"
+         2 "Turn ON"
+         3 "Turn OFF")
+
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            exit
+            ;;
+        2)
+            cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
+            sed -i '/^--flashattention/d' altaera.sh
+            echo "--flashattention" >> altaera.sh
+            cd '/data/data/com.termux/files/home'
+            ;;
+        3)
+            cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
+            sed -i '/^--flashattention/d' altaera.sh
+            cd '/data/data/com.termux/files/home'
+            ;;
+esac
