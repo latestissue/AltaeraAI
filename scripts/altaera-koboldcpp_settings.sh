c99b20437a135560a0635476eaa421f8633483ab
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-02 15:58:58 +0200

    Update altaera-koboldcpp_settings.sh

--- scripts/altaera-koboldcpp_settings.sh
+++ scripts/altaera-koboldcpp_settings.sh
@@ -9,7 +9,8 @@ MENU="What would you like to change?"
 
 OPTIONS=(1 "[...] Go Back"
          2 "Context Size [...]"
-         3 "Blas Batch Size [...]")
+         3 "Blas Batch Size [...]"
+	 4 "Flash Attention (Experimental) [...]")
          
 
 CHOICE=$(dialog --clear \
@@ -33,6 +34,11 @@ case $CHOICE in
         3)
             clear
             './AltaeraAI/altaera-koboldcpp_settings_blasbatchsize.sh'
+            './AltaeraAI/altaera-koboldcpp_settings.sh'
+            ;;
+        4)
+            clear
+            './AltaeraAI/altaera-koboldcpp_settings_flashattention.sh'
             './AltaeraAI/altaera-koboldcpp_settings.sh'
             ;;
 esac
