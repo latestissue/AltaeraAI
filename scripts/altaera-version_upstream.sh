e5d1a37a9e3409edf043c212447801fc928d0252
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-05-10 14:04:29 +0200

    v4.8.1 >> v4.8.2

--- scripts/altaera-version_upstream.sh
+++ scripts/altaera-version_upstream.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "v4.8.1"
+echo "v4.8.2"
 
 exit
--- scripts/altaera_pol.sh
+++ scripts/altaera_pol.sh
@@ -5,7 +5,7 @@ cd '/data/data/com.termux/files/home'
 HEIGHT=30
 WIDTH=60
 CHOICE_HEIGHT=7
-BACKTITLE="AltaeraAI - v4.8.1"
+BACKTITLE="AltaeraAI - v4.8.2"
 TITLE="Witaj!"
 MENU="Co chciałbyś zrobić?"
 
