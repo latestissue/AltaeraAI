25d768b705cfdda3955fdcf2a066c5b430b7553c
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-10 14:21:18 +0200

    Create altaera-auto-update-check.sh

---
+++ scripts/altaera-auto-update-check.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+cd '/data/data/com.termux/files/home'
+
+        echo "Automatically checking for updates...";
+        
+                {
+                cd "AltaeraAI"
+                wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-version_upstream.sh
+                chmod a+x 'altaera-version_upstream.sh'
+                cd ..
+                } &> /dev/null 2>&1;
+ 
+         if [ $(bash 'AltaeraAI/altaera-version_upstream.sh'
+          ) = "v4.8.2" ]; then
+          rm -rf 'AltaeraAI/altaera-version_upstream.sh'
+          else
+          rm -rf 'AltaeraAI/altaera-version_upstream.sh'
+          bash 'AltaeraAI/altaera-update_available.sh'
+        fi
+
+HEIGHT=20
+WIDTH=60
+CHOICE_HEIGHT=7
+BACKTITLE="AltaeraAI - v4.8.2 | RAM:$(free -m | awk '/^Mem:/{printf("%.1fG\n",$2/1000)}') | Free Storage:$(df -Ph . | tail -1 | awk '{print $4}')"
+TITLE="Welcome!"
+MENU="What would you like to do?"
+
+OPTIONS=(1 "Start AltaeraAI [KoboldCpp]"
+         2 "Check for updates [...]"
+         3 "Force update [...]"
+         4 "Manage AI Models 🤖 [...]"
+         5 "Change language 🌐"
+         6 "Change KoboldCpp Settings [...]"
+         7 "ngrok Secure Tunnelling [...]"
+         8 "Reinstall (fix) AltaeraAI [...]"
+         9 "Uninstall AltaeraAI"
+         10 "Customer Support [...]"
+         11 "Visit altaera.ai [URL]"
+         12 "Donate 💸 [URL]"
+         13 "Exit")
+         
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            clear
+            proot-distro login altaera -- ./altaera.sh
+            ;;
+        2)
+            clear
+            './AltaeraAI/altaera-check_for_updates.sh'
+            './AltaeraAI/altaera.sh'
+            ;;
+        3)
+            clear
+            './AltaeraAI/altaera-force_update.sh'
+            './AltaeraAI/altaera.sh'
+            ;;
+        4)
+            clear
+            './AltaeraAI/altaera-manage_models.sh'
+            './AltaeraAI/altaera.sh'
+            ;;
+        5)
+            clear
+            './AltaeraAI/altaera-lang.sh'
+            './AltaeraAI/altaera.sh'
+            ;;
+        6)
+            clear
+            './AltaeraAI/altaera-koboldcpp_settings.sh'
+            './AltaeraAI/altaera.sh'
+            ;;
+        7)
+            clear
+            './AltaeraAI/altaera-ngrok.sh'
+            './AltaeraAI/altaera.sh'
+            ;;
+        8)
+            clear
+            './AltaeraAI/altaera-reinstall.sh'
+            ;;
+        9)
+            clear
+            './AltaeraAI/altaera-uninstall.sh'
+            ;;
+        10)
+            clear
+            './AltaeraAI/altaera-customer_support.sh'
+            ;;
+        11)
+            clear
+            termux-open-url 'https://altaera.ai/'
+            ;;
+        12)
+            clear
+            termux-open-url 'https://altaera.ai/donate/'
+            ;;
+        13)
+            clear
+            exit
+esac
