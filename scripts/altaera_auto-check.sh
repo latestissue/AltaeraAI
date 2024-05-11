ac8b276bb39522aabb899d214e26042f523a65d0
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-11 10:42:04 +0200

    Update altaera_auto-check.sh

--- scripts/altaera_auto-check.sh
+++ scripts/altaera_auto-check.sh
@@ -1,5 +1,9 @@
 clear
 
+if
+         [ $(cat < /dev/null > /dev/tcp/8.8.8.8/53; echo $?
+          ) = "0" ]; then
+          
         echo "Automatically checking for updates...";
 
                 {
@@ -9,25 +13,21 @@ clear
                 cd ..
                 } &> /dev/null 2>&1;
 
-if
-         [ $(cat < /dev/null > /dev/tcp/8.8.8.8/53; echo $?
-          ) = "0" ]; then
 if [ $(bash 'AltaeraAI/altaera-version_upstream.sh'
           ) = "v4.8.4" ]; then
           rm -rf 'AltaeraAI/altaera-version_upstream.sh'
-elif
-[ $(cat < /dev/null > /dev/tcp/8.8.8.8/53; echo $?
-          ) = "bash: connect: Network is unreachable                   bash: /dev/tcp/8.8.8.8/53: Network is unreachable       1" ]; then
+else
+          rm -rf 'AltaeraAI/altaera-version_upstream.sh'
+          bash 'AltaeraAI/altaera-auto-update_available.sh'
+          fi
+                   
 rm -rf 'AltaeraAI/altaera-version_upstream.sh'
 fi
-else
-rm -rf 'AltaeraAI/altaera-version_upstream.sh'
+
 
 ###         if [ $(bash 'AltaeraAI/altaera-version_upst>
    ###       ) = "v4.8.4" ]; then
       ###    rm -rf 'AltaeraAI/altaera-version_upstream>
-fi
-else
-          rm -rf 'AltaeraAI/altaera-version_upstream.sh'
-          bash 'AltaeraAI/altaera-auto-update_available.sh'
-        fi
+
+
+        
