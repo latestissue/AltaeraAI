0dea34f78d4024f8d496f6d51185fd3b2a858c2b
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-12 14:58:48 +0200

    Create altaera-update_no-check.sh

---
+++ scripts/altaera-update_no-check.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+HEIGHT=20
+WIDTH=100
+CHOICE_HEIGHT=24
+BACKTITLE="AltaeraAI - Update"
+TITLE="Choosing updating method"
+MENU="Would you like to update with the pre-packaged KoboldCpp [Faster], or by building your own? [Slower]"
+
+OPTIONS=(1 "Update with pre-packaged KoboldCpp [Faster]"
+         2 "Update with building your own KoboldCpp [Slower]"
+         3 "Update with building your own KoboldCpp (No-Blas) [Slower]"
+         4 "Update shell files only")
+
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+
+        1)
+            clear
+            echo "Initializing update...
+            
+            ";
+            {
+            rm -rf 'altaera-update_content.sh'
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content-fast_no-check.sh -O 'altaera-update_content.sh'
+            chmod a+x 'altaera-update_content.sh'
+            } &> /dev/null 2>&1;
+            bash 'altaera-update_content.sh'
+            clear
+            bash 'AltaeraAI/altaera-updated_successfully.sh'
+            ;;
+        2)
+            clear
+            echo "Initializing update...
+            
+            ";
+            {
+            rm -rf 'altaera-update_content.sh'
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content-slow_no-check.sh -O 'altaera-update_content.sh'
+            chmod a+x 'altaera-update_content.sh'
+            } &> /dev/null 2>&1;
+            bash 'altaera-update_content.sh'
+            clear
+            bash 'AltaeraAI/altaera-updated_successfully.sh'
+            ;;
+        3)
+            clear
+            echo "Initializing update...
+            
+            ";
+            {
+            rm -rf 'altaera-update_content.sh'
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content-slow_no-blas_no-check.sh -O 'altaera-update_content.sh'
+            chmod a+x 'altaera-update_content.sh'
+            } &> /dev/null 2>&1;
+            bash 'altaera-update_content.sh'
+            clear
+            bash 'AltaeraAI/altaera-updated_successfully.sh'
+            ;;
+        4)
+            clear
+            echo "Initializing update...
+            
+            ";
+            {
+            rm -rf 'altaera-update_content.sh'
+            wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-update_content-scripts_no-check.sh -O 'altaera-update_content.sh'
+            chmod a+x 'altaera-update_content.sh'
+            } &> /dev/null 2>&1;
+            bash 'altaera-update_content.sh'
+            clear
+            bash 'AltaeraAI/altaera-updated_successfully.sh'
+            ;;
+            
+esac
