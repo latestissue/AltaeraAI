57613caa5b7602f73367016f805d9cf5b6876fbd
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-12 13:46:05 +0200

    Update altaera-changelog_cli.sh

--- scripts/altaera-changelog_cli.sh
+++ scripts/altaera-changelog_cli.sh
@@ -3,6 +3,7 @@
 dialog --backtitle "AltaeraAI" \
 --title "AltaeraAI - Changelog [CLI]" \
 --msgbox '
+    * v4.9 – updated to koboldcpp-1.65
     * v4.8.5 - fixed a bug which always informed the user about an available update, when launching AltaeraAI in offline mode
     * v4.8.4 – added a changelog to the main MENU
     * v4.8.3 – added an optional (set by default) “auto-update” mechanism, which automatically
