36ce1826536469e50bfe0782b7923bba65170620
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-05-13 15:39:32 +0200

    add "benchmark" mode

--- scripts/altaera-update_content_chn-slow.sh
+++ scripts/altaera-update_content_chn-slow.sh
@@ -242,6 +242,9 @@ rm -rf 'klite.embd'
 rm -rf 'altaera.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-proot/altaera.sh
 chmod a+x 'altaera.sh'
+rm -rf 'benchmark.sh'
+wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-proot/benchmark.sh
+chmod a+x 'benchmark.sh'
 rm -rf 'altaera-model_list.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-proot/altaera-model_list.sh
 chmod a+x 'altaera-model_list.sh'
