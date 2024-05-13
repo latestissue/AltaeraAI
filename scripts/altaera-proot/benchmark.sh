bbe54efb2137bfce2c327e786ee684b28d1eeb13
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-13 15:37:34 +0200

    Update benchmark.sh

--- scripts/altaera-proot/benchmark.sh
+++ scripts/altaera-proot/benchmark.sh
@@ -11,7 +11,7 @@ if [ ${#files[@]} -eq 0 ]; then
     clear
     echo "No .bin or .gguf files found in $directory"
 else
-    file=$(dialog --stdout --title "Select an AI Model:" --menu "Choose a file:" 0 0 0 "${files[@]}")
+    file=$(dialog --stdout --title "Select an AI Model to benchmark:" --menu "Choose a file:" 0 0 0 "${files[@]}")
 fi
 
 RESULT=$?
