aea6df731b02cf1966c78256c4d67af67e64dc42
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-16 14:28:40 +0200

    Update altaera-update_content-scripts_no-check.sh

--- scripts/altaera-update_content-scripts_no-check.sh
+++ scripts/altaera-update_content-scripts_no-check.sh
@@ -172,7 +172,7 @@ chmod a+x 'altaera-model_restore.sh'
 
 } &> /dev/null 2>&1;
 
-echo "Updating AltaeraAI PRoot Distro environment (Artix Linux)...";
+echo "Updating AltaeraAI PRoot Distro environment (Artix Linux)..." | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols); | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
 
 {
 proot-distro login altaera -- ./upgrade.sh &
