3764ba192f174df9f714f6c248218ea777ff3b02
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-17 13:24:25 +0200

    Update bashtest.sh

--- scripts/bashtest.sh
+++ scripts/bashtest.sh
@@ -7,7 +7,7 @@ BACKTITLE="AltaeraAI - AI Model Settings"
 TITLE="AI Model Selection"
 MENU="Choose your desired Gemma-2B-it GGUF model size/strategy:"
 
-OPTIONS=(1 "Gemma Terms of Use [URL]"
+OPTIONS=(1 "Gemma Terms of Use [URL] - [must read before]"
          2 "gemma-2b-it.Q2_K.gguf"
          2 "gemma-2b-it.Q3_K_L.gguf"
          2 "gemma-2b-it.Q3_K_M.gguf"
@@ -42,13 +42,15 @@ case $CHOICE in
             dialog --title "Gemma-2B-it GGUF" \
         --backtitle "AltaeraAI - Notice" \
         --yesno "Gemma is provided under and subject to the Gemma Terms of Use found at ai.google.dev/gemma/terms
-By confirming, you are stating that you accept Terms of Use provided in the previous window, as well as the Use Restrictions found in Section 3.2 of the attached URL link" 7 60
+
+By confirming, you are stating that you accept Terms of Use provided in the previous window, as well as the Use Restrictions found in Section 3.2 of the attached URL link" 10 60
 
 response=$?
 case $response in
 
         0)      
-                    echo "You chose 'Q3_K_L-llama-2-7b-chat'
+                    clear
+                    echo "You chose 'gemma-2b-it.Q2_K.gguf'
                         
             To abort download, press 'Ctrl+C'
             [don't forget to delete the file afterwards]
@@ -65,10 +67,13 @@ case $response in
             clear
         
           ;;
-        1) ./data/data/com.termux/files/home/AltaeraAI/altaera.sh;;
-        255) ./data/data/com.termux/files/home/AltaeraAI/altaera.sh;;
-esac
-
+        1)
+          clear
+          ./data/data/com.termux/files/home/AltaeraAI/altaera-model.sh;;
+        255)
+          clear
+          ./data/data/com.termux/files/home/AltaeraAI/altaera-model.sh;;
+        esac
             ;;
         2)
             echo "You chose 'Q3_K_L-llama-2-7b-chat'
