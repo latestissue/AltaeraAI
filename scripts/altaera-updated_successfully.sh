ad2e031409150d2eafbac7ed0b7511db7ce38bfb
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-19 18:14:46 +0200

    Update altaera-updated_successfully.sh

--- scripts/altaera-updated_successfully.sh
+++ scripts/altaera-updated_successfully.sh
@@ -3,7 +3,10 @@
 dialog --backtitle "AltaeraAI" \
 --title "AltaeraAI - Update" \
 --msgbox 'AltaeraAI has been updated succesfully!
+
+
 Recent changes:
+
     * v4.9.4 - added an optional (set by default) black MENU background (Bash display dialog boxes)
     * v4.9.3 – added Gemma-2B/7B-it weights (and a reference to their LICENSE file, with a notice) to the list of models
     * v4.9.2 - added Yi-1.5-6B-Chat weights to the list of models
