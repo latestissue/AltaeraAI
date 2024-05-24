2c70f3bec7aac214a232852f87bd465e658f6937
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-24 22:58:48 +0200

    Update altaera-changelog_cli.sh

--- scripts/altaera-changelog_cli.sh
+++ scripts/altaera-changelog_cli.sh
@@ -3,8 +3,9 @@
 dialog --backtitle "AltaeraAI" \
 --title "AltaeraAI - Changelog [CLI]" \
 --msgbox '
-    * v4.9.6 - fixed "KoboldCpp Settings"
-    * v4.9.6 - added an optional (set by default) black MENU background (Bash display dialog boxes)
+    * v4.9.6 - added Phi-SoSerious-Mini-V1/imatrix weights to the list of models
+    * v4.9.5 - fixed "KoboldCpp Settings"
+    * v4.9.4 - added an optional (set by default) black MENU background (Bash display dialog boxes)
     * v4.9.3 – added Gemma-2B/7B-it weights (and a reference to their LICENSE file, with a notice) to the list of models
     * v4.9.2 - added Yi-1.5-6B-Chat weights to the list of models
     * v4.9.1 - added "Benchmark" mode to test AI models (--benchmark flag - KoboldCpp), into the MENU
