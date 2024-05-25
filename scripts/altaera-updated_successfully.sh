14344918f7cec0a66970b12a0647032ce84f944d
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-25 12:56:31 +0200

    Update altaera-updated_successfully.sh

--- scripts/altaera-updated_successfully.sh
+++ scripts/altaera-updated_successfully.sh
@@ -7,11 +7,11 @@ dialog --backtitle "AltaeraAI" \
 
 Recent changes:
 
-    * v5.0 - added an optional (set by default) black MENU background (Bash display dialog boxes)
+    * v5.0 - updated to koboldcpp-1.66.1
+    * v4.9.6 - added Phi-SoSerious-Mini-V1/imatrix weights to the list of models
+    * v4.9.5 - fixed "KoboldCpp Settings"
+    * v4.9.4 - added an optional (set by default) black MENU background (Bash display dialog boxes)
     * v4.9.3 – added Gemma-2B/7B-it weights (and a reference to their LICENSE file, with a notice) to the list of models
-    * v4.9.2 - added Yi-1.5-6B-Chat weights to the list of models
-    * v4.9.1 - added "Benchmark" mode to test AI models (--benchmark flag - KoboldCpp), into the MENU
-    * v4.9 – updated to koboldcpp-1.65
 ' 25 40
 
 bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
