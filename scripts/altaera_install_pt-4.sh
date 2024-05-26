f3b22410e98277baad85b020bcedc1c87a8e179d
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-26 15:52:02 +0200

    Update altaera_install_pt-4.sh

--- scripts/altaera_install_pt-4.sh
+++ scripts/altaera_install_pt-4.sh
@@ -1,6 +1,49 @@
 #!/bin/bash
-clear
-echo "Installing dependencies...";
+
+            clear
+            echo "
+
+        ██████████
+       ██ █
+      ██  █
+     ██   █
+     █    ████████
+    ██    █
+   ████████
+  ██      █
+  █       ████████
+
+  AltaeraAI - v5.0
+
+  "
+
+  echo "Did you know...
+
+  " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+
+  	function random_message() {
+    # Array of random messages
+    messages=("that KobbleTiny is the world's sweetest child?" "that KobbleTiny is concedo's designed mind?")
+
+    # Generate a random index
+    index=$(( RANDOM % ${#messages[@]} ))
+
+    # Get the random message
+    message=${messages[$index]}
+
+    # Print the random message
+    echo $message | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+}
+
+# Call the function
+random_message
+
+echo "________________________________________________________________
+" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+
+echo "Installing dependencies...;
+
+" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
 
 {
 cd '/root'
@@ -15,7 +58,9 @@ chmod a+x 'altaera_install-architecture.sh'
 bash 'altaera_install-architecture.sh'
 } &> /dev/null 2>&1;
 
-echo "Cleaning up File System...";
+echo "Cleaning up File System...;
+
+" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
 
 {
 rm -rf 'chat-with-bob.txt'
