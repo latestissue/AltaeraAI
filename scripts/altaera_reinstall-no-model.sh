b45bc0d135b3bf546c4ccb7ec3cf4bb6fb763592
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-31 13:20:13 +0200

    Update altaera_reinstall-no-model.sh

--- scripts/altaera_reinstall-no-model.sh
+++ scripts/altaera_reinstall-no-model.sh
@@ -37,10 +37,8 @@ case $CHOICE in
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_install_pt-2.sh -q --show-progress
 chmod a+x 'altaera_install_pt-2.sh'
 bash 'altaera_install_pt-2.sh'
-{
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_reinstall-scripts.sh
 chmod a+x 'altaera_reinstall-scripts.sh'
-} &> /dev/null 2>&1;
 bash 'altaera_reinstall-scripts.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-install_method.sh -q --show-progress
 chmod a+x 'altaera-install_method.sh'
