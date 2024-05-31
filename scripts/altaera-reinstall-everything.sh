bbb59ef1bcc99eadc6c5ce8b521087547331d272
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-05-31 11:57:51 +0200

    Update altaera-reinstall-everything.sh

--- scripts/altaera-reinstall-everything.sh
+++ scripts/altaera-reinstall-everything.sh
@@ -42,7 +42,7 @@ chmod a+x 'altaera_install_pt-2.sh'
 bash 'altaera_install_pt-2.sh'
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera_reinstall-scripts.sh
 chmod a+x 'altaera_reinstall-scripts.sh'
-bash 'altaera_reinstall-scripts.sh'
+bash 'altaera_reinstall-scripts.sh' | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
 wget https://raw.githubusercontent.com/latestissue/AltaeraAI/main/scripts/altaera-install_method.sh -q --show-progress
 chmod a+x 'altaera-install_method.sh'
 bash 'altaera-install_method.sh'
