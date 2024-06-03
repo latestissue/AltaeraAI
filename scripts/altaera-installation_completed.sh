f2548d640f75d8c09b01695b5bead11cb9064b9e
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-06-03 12:25:58 +0200

    Create altaera-installation_completed.sh

---
+++ scripts/altaera-installation_completed.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dialog --backtitle "AltaeraAI" \
+--title "AltaeraAI - Installation" \
+--msgbox 'Installation completed - press Enter to continue.
+      --- Then, open Termux and:
+      
+- To start, please type in "ae".' 10 30
+
+exit
