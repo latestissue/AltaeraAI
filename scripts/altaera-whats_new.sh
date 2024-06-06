9771b942d7f986d33fe3194fa99757eb7a0af6f6
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-06-06 14:32:52 +0200

    Update altaera-whats_new.sh

--- scripts/altaera-whats_new.sh
+++ scripts/altaera-whats_new.sh
@@ -3,12 +3,13 @@
 dialog --backtitle "AltaeraAI" \
 --title "AltaeraAI - Changelog [CLI]" \
 --msgbox '
+    * v5.1 - updated to koboldcpp-1.67
+    * added "aef" and "aeforce" arguments to the "bash.bashrc" file, which allow the user to launch AltaeraAI without the automatic update checking mechanism, in case there is a start-up problem, i.e., poor network connectivity
     * v5.0 - updated to koboldcpp-1.66.1
     * visual fixes and improvements to Model MENUS
     * v4.9.6 - added Phi-SoSerious-Mini-V1/imatrix weights to the list of models
     * v4.9.5 - fixed "KoboldCpp Settings"
     * v4.9.4 - added an optional (set by default) black MENU background (Bash display dialog boxes)
-    * v4.9.3 – added Gemma-2B/7B-it weights (and a reference to their LICENSE file, with a notice) to the list of models
 ' 100 100
 
 exit
