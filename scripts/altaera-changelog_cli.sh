51ada10e43115a3fd12068823c982e7aee3842b8
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-06-06 14:32:32 +0200

    Update altaera-changelog_cli.sh

--- scripts/altaera-changelog_cli.sh
+++ scripts/altaera-changelog_cli.sh
@@ -3,6 +3,8 @@
 dialog --backtitle "AltaeraAI" \
 --title "AltaeraAI - Changelog [CLI]" \
 --msgbox '
+    * v5.1 - updated to koboldcpp-1.67
+    * added "aef" and "aeforce" arguments to the "bash.bashrc" file, which allow the user to launch AltaeraAI without the automatic update checking mechanism, in case there is a start-up problem, i.e., poor network connectivity
     * v5.0 - updated to koboldcpp-1.66.1
     * visual fixes and improvements to Model MENUS
     * v4.9.6 - added Phi-SoSerious-Mini-V1/imatrix weights to the list of models
