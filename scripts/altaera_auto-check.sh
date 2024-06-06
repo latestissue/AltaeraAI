75b01b8dfb2283e14642ae04d2a654dd6e6c33c6
Krystian Węgrzyniak <krystianwegrzyniak@protonmail.com>
2024-06-06 14:41:10 +0200

    Update altaera_auto-check.sh

--- scripts/altaera_auto-check.sh
+++ scripts/altaera_auto-check.sh
@@ -14,7 +14,7 @@ if
                 } &> /dev/null 2>&1;
 
 if [ $(bash 'AltaeraAI/altaera-version_upstream.sh'
-          ) = "v5.0" ]; then
+          ) = "v5.1" ]; then
           rm -rf 'AltaeraAI/altaera-version_upstream.sh'
 else
           rm -rf 'AltaeraAI/altaera-version_upstream.sh'
